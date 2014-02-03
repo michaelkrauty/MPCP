75684494b7b11d470f2687d53db60d20a7b0ce21
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-03 15:25:47 -0800

    Update index.php

--- querytest/index.php
+++ querytest/index.php
@@ -4,13 +4,9 @@
 	$status = new MinecraftServerStatus();
 	$response = $status->getStatus('pvp24.com');
 	if(!$response) {
-		echo"The Server is offline!";
+		echo "The Server is Offline!";
 	} else {
-		echo"<img width=\"64\" height=\"64\" src=\"".$response['favicon']."\" /> <br>
-		The Server ".$response['hostname']." is running on ".$response['version']." and is online,
-		currently are ".$response['players']." players online
-		of a maximum of ".$response['maxplayers'].". The motd of the server is '".$response['motd']."'. 
-		The server has a ping of ".$response['ping']." milliseconds.";
+		echo "The Server is Online!"
 	}
 
 ?>
