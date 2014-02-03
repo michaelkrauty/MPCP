64e5c69b3d90895a7d35bbcb3ee5e80ce4a54b7b
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-03 15:25:07 -0800

    Update index.php

--- querytest/index.php
+++ querytest/index.php
@@ -1,13 +1,16 @@
-<?php include "../mpcp.php";
-#  $serverIp = "carbon.mc.gl:25565";
-  funciton getStatus("carbon.mc.gl"){
-    include_once 'MinecraftServerStatus/status.class.php';
-    $status = new MinecraftServerStatus();
-    $response = $status->getStatus("carbon.mc.gl");
-    if(!$response){
-      echo "offline";
-    }else{
-      echo "online";
-    }
-  }
+<?php
+  include_once "../mpcp.php";
+	include_once 'MinecraftServerStatus/status.class.php';
+	$status = new MinecraftServerStatus();
+	$response = $status->getStatus('pvp24.com');
+	if(!$response) {
+		echo"The Server is offline!";
+	} else {
+		echo"<img width=\"64\" height=\"64\" src=\"".$response['favicon']."\" /> <br>
+		The Server ".$response['hostname']." is running on ".$response['version']." and is online,
+		currently are ".$response['players']." players online
+		of a maximum of ".$response['maxplayers'].". The motd of the server is '".$response['motd']."'. 
+		The server has a ping of ".$response['ping']." milliseconds.";
+	}
+
 ?>
