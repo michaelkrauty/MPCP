5cbe34a9023a1baba2711deac9bbe53396e71341
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-03 15:20:26 -0800

    Update index.php

--- querytest/index.php
+++ querytest/index.php
@@ -1,5 +1,5 @@
 <?php include "../mpcp.php";
-  $serverIp = "carbon.mc.gl";
+  $serverIp = "carbon.mc.gl:25565";
   funciton getStatus($serverIp){
     include_once 'MinecraftServerStatus/status.class.php';
     $status = new MinecraftServerStatus();
