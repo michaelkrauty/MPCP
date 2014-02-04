784822ae4efdea3d1b93894481bcbfd940ec0c43
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-04 07:42:37 +0000

    modified:   login.php

--- login.php
+++ login.php
@@ -20,7 +20,7 @@
     	<div class="container">
 	  	  <div class="well">
 	  	    <form action="includes/process_login.php" class="form-signin" role="form" method="post" name="login_form">
-	  	    	<center><logintitle>Sign In</logintitle></center>
+	  	    	<logintitle>Sign In</logintitle>
 		      	<?php
 	  	    	  if (isset($_GET['error'])) {
 	        	  	echo '<p class="error">Error Logging In!</p>';
