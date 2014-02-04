9268f245322236d947e3c4ce2f739286a2b0a6de
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-03 16:24:37 -0800

    Update login.php

--- login.php
+++ login.php
@@ -37,7 +37,7 @@
         <input type="email" class="form-control" placeholder="Email address" required autofocus>
         <input type="password" class="form-control" placeholder="Password" required>
         <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
+          <input type="checkbox" value="remember-me"> <font color="gray">Remember me</font>
         </label>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       </form>
