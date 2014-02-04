0b841d4c7c01cef8adb64b0c430c9e9cacd6b1fa
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-04 06:02:11 +0000

    new file:   includes/logout.php

---
+++ includes/logout.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ *
+ * ############################################
+ * Logout Processor
+ * ############################################
+ *
+ * DESCRIPTION
+ *
+ * Processes Logout Request
+ *
+ * TABLE OF CONTENTS
+ *
+ * 1) Includes
+ * 2) Tear Down Session
+ *
+ * @ Copyright (c) MPCP
+ * @ Version
+ * @ Author - Michael Krautkramer
+ * @ Author - Daniel Oxenbury (http://Daniel.oxituk.co.uk)
+*/
+
+
+/*
+* 1) Includes
+*/
+include_once 'includes/functions.php';
+/*
+* --------------------------
+*/
+
+
+/*
+* 2) Tear Down Session
+*/
+
+sec_session_start();
+
+// Unset all session values
+$_SESSION = array();
+
+// get session parameters
+$params = session_get_cookie_params();
+
+// Delete the actual cookie.
+setcookie(session_name(),_ '', time() - 42000,$params["path"],$params["domain"],$params["secure"],$params["httponly"]);
+
+// Destroy session
+session_destroy();
+header('Location: ../ index.php');
+
+/*
+* --------------------------
+*/
+
