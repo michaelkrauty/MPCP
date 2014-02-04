5343ee22cb9f7ff3e6a3a4b45cae3b386220bc96
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-04 14:52:15 -0800

    Update index.php

--- index.php
+++ index.php
@@ -75,7 +75,6 @@
       <div class='page-header'>
         <center>
           <h1>Welcome, <?php echo $userName; ?>!</h1>
-          <h2><?php echo $userName; echo $userEmail; ?></h2>
           <?php
           $image1 = "http://signaturecraft.us/avatars/30/face/" . $userName . ".png";
           $image2 = "http://signaturecraft.us/avatars/30/face/noskin.png";
