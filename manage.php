52ddbe8e23d0720de00f2f2efe70385b1050efba
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-03 21:36:55 -0800

    Update manage.php

--- manage.php
+++ manage.php
@@ -51,7 +51,7 @@
           </ul>
           <ul class="nav navbar-nav navbar-right">
               <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                <a href="" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
                 <ul class="dropdown-menu">
                   <li><a href="#">Action</a></li>
                   <li><a href="#">Another action</a></li>
