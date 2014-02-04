4ed6461620f3b0e72bea4dab54881ad27566a787
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-04 14:42:52 -0800

    Update index.php

--- index.php
+++ index.php
@@ -82,9 +82,9 @@
             $md5image1 = md5(file_get_contents($image1));
             $md5image2 = md5(file_get_contents($image2));
             if($md5image1 == $md5image2){
-          <img src="http://signaturecraft.us/avatars/30/face/Herobrine.png" height="100" width="100">
+              echo "<img src='http://signaturecraft.us/avatars/30/face/Herobrine.png' height='100' width='100'>";
             }else{
-          <img src="http://signaturecraft.us/avatars/30/face/<?php echo $userName; ?>.png" height="100" width="100">
+              echo "<img src='http://signaturecraft.us/avatars/30/face/" . $userName . ".png' height='100' width='100'>";
             }
           ?>
         </center>
