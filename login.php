beb5ca62749a1321bc2bb184cf2c2c49b4e964a9
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-03 23:17:56 -0800

    Update login.php

--- login.php
+++ login.php
@@ -1,16 +1,4 @@
-<?php
-include_once 'includes/db_connect.php';
-include_once 'includes/functions.php';
-
-sec_session_start();
-
-if (login_check($mysqli) == true) {
-    $logged = 'in';
-} else {
-    $logged = 'out';
-}
-?>
-
+<?php include_once 'includes/db_connect.php';	include_once 'includes/functions.php'; sec_session_start();	if (login_check($mysqli) == true) {	$logged = 'in';	} else { $logged = 'out';	} ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
