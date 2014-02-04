c2b878b7d40a6fdd317e3e5d12a528c80e7ae78b
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-04 05:48:00 +0000

    modified:   includes/functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -35,28 +35,27 @@ include_once 'mpcp-config.php';
 /*
 * 2) Secure Session Function
 */
-function sec_session_start(){
-	$session_name = 'sec_session_id'; // Session Name.
-	$secure = SECURE; // Stops JS from accessing session id.
-	$httponly = true;
-/*
-	// Forces session to only use cookies.
-	if (ini_set('session.use_only_cookies', 1) === FALSE) {
-		header("Location: ../error.php?err=Could not initiate a safe session (ini_set)");
-		exit();
-	}
-*/
-	// Gets current cookies params.
-	$cookieParams = session_get_cookie_params();
-	session_set_cookie_params($cookieParams["lifetime"],
-		$cookieParams["path"],
-		$cookieParams["domain"],
-		$secure,
-		$httponly);
-
-	session_name($session_name); // Sets the session name to the one set above.
-	session_start(); // Start the PHP session.
-	session_regenerate_id(); // Delete old session and regenrate.
+function sec_session_start() {
+    $session_name = 'sec_session_id';   // Set a custom session name
+    $secure = SECURE;
+    // This stops JavaScript being able to access the session id.
+    $httponly = true;
+    // Forces sessions to only use cookies.
+    if (ini_set('session.use_only_cookies', 1) === FALSE) {
+        header("Location: ../error.php?err=Could not initiate a safe session (ini_set)");
+        exit();
+    }
+    // Gets current cookies params.
+    $cookieParams = session_get_cookie_params();
+    session_set_cookie_params($cookieParams["lifetime"],
+        $cookieParams["path"], 
+        $cookieParams["domain"], 
+        $secure,
+        $httponly);
+    // Sets the session name to the one set above.
+    session_name($session_name);
+    session_start();            // Start the PHP session 
+    session_regenerate_id();    // regenerated the session, delete the old one. 
 }
 /*
 * --------------------------
