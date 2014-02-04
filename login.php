4ea6c8d4049f48732577ee1eeb79101000d2b40c
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-04 00:17:14 -0800

    Update login.php

--- login.php
+++ login.php
@@ -32,7 +32,10 @@
 	      		<input type="password" name="password" class="form-control" placeholder="Password" required>
 	      		<label class="checkbox">
 	      		<input type="checkbox" value="remember-me">Remember me</label>
-	      		<input class= "btn btn-lg btn-primary btn-block" type="button" value="Login" onclick="formhash(this.form, this.form.password);" />
+	      		<input class="btn btn-lg btn-primary btn-block" type="button" value="Login" onclick="formhash(this.form, this.form.password);" />
+	      		<br>
+	      		<input class="btn btn-lg btn-success btn-block" type="button" value="Register">
+	      		<input class="btn btn-lg btn-warning btn-block" type="button" value="Forgot Password">
 					</form>
 				</div>
 			</div>
