e1dfa9705b8855e343a9e1c898ad963c7aaad9b5
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-03 16:28:13 -0800

    Update login.php

--- login.php
+++ login.php
@@ -29,11 +29,10 @@
   </head>
 
   <body>
-    <center><pagetitle>Login:</pagetitle></center>
     <br>
     <div class="container">
       <form class="form-signin" role="form">
-      <!--<h2 class="form-signin-heading">Please sign in</h2>-->
+      <h2 class="form-signin-heading">Sign in:</h2>
         <input type="email" class="form-control" placeholder="Email address" required autofocus>
         <input type="password" class="form-control" placeholder="Password" required>
         <label class="checkbox">
