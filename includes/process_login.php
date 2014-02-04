370e01dc905dfc72d4e5c4e44bb986dd1108b37b
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-04 05:42:51 +0000

    modified:   includes/process_login.php 	new file:   protected_page.php 	deleted:    dropdown.php 	deleted:    webroot/css/navbar.css

--- includes/process_login.php
+++ includes/process_login.php
@@ -48,7 +48,7 @@ sec_session_start();
     		$password = $_POST['p']; // The hashed password.
 		if (login($email, $password, $mysqli) == true) {
        			// Login success 
-        		header('Location: ../login.php');
+        		header('Location: ../protected_page.php');
    		} else {
 		        // Login failed 
         		header('Location: ../index.php?error=1');
