d4f87c4ed3c5ae38eeb470a98344b8b10de3f076
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-04 06:03:41 +0000

    modified:   includes/logout.php

--- includes/logout.php
+++ includes/logout.php
@@ -33,22 +33,23 @@ include_once 'includes/functions.php';
 /*
 * 2) Tear Down Session
 */
-
-sec_session_start();
-
-// Unset all session values
+// Unset all session values 
 $_SESSION = array();
-
-// get session parameters
+ 
+// get session parameters 
 $params = session_get_cookie_params();
-
-// Delete the actual cookie.
-setcookie(session_name(),_ '', time() - 42000,$params["path"],$params["domain"],$params["secure"],$params["httponly"]);
-
-// Destroy session
+ 
+// Delete the actual cookie. 
+setcookie(session_name(),
+        '', time() - 42000, 
+        $params["path"], 
+        $params["domain"], 
+        $params["secure"], 
+        $params["httponly"]);
+ 
+// Destroy session 
 session_destroy();
 header('Location: ../ index.php');
-
 /*
 * --------------------------
 */
