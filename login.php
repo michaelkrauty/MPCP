29586ea2ae5005d7c9e09ba0ae7cdd75b04237ed
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-04 23:54:10 +0000

    modified:   login.php

--- login.php
+++ login.php
@@ -49,7 +49,7 @@
 			<tr>
 			<th width="45%">
   			<input class="btn btn-lg btn-success btn-block" type="button" value="Register" onClick="parent.location='register.php'">
-     			<th width="10%>
+     			<th width="10%">
 			<th width="45%">
 			<input class="btn btn-lg btn-warning btn-block" type="button" value="Forgot Password" onClick="parent.location='password_lost.php'">
 			</tr>
