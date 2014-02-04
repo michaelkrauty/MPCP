a1961d0ae53b436429bb18b28567ac1a4b8fbb6c
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-04 06:00:29 +0000

    modified:   includes/functions.php 	modified:   includes/process_login.php

--- includes/functions.php
+++ includes/functions.php
@@ -159,40 +159,53 @@ function checkbrute($user_id, $mysqli) {
 /*
 * 4) Login Check
 */
-
-function login_check ($mysqli) {
-
-	if (isset($_SESSION['user_id'],	$_SESSION['username'], $_SESSION['loggin_string'])) {
-		$user_id = $_SESSION['user_id'];
-		$login_string = $_SESSION['login_string'];
-		$username = $_SESSION['username'];
-
-	$user_browser = $_SERVER['HTTP_USER_AGENT'];
-
-	if ($stmt = $stmt = $mysqli->prepare("SELECT password FROM members WHERE id = ? LIMIT 1")) {
-		$stmt->bind_param('i', $user_id);
-		$stmt->execute();
-		$stmt->store_result();
-
-		if ($stmt->num_rows == 1) {
-			$stmt->bind_result($password);
-			$stmt->fetch();
-			$login_check = hash('sha512', $password . $user_browser);
-
-			if ($login_check == $login_string) {
-				return true; // Logged In.
-			} else {
-				return false; // Not Logged In.
-			}
-		} else {
-		return false; // Not Logged In.
-		}
-	} else {
-	return false; // Not Logged In.
-	}
+function login_check($mysqli) {
+    // Check if all session variables are set 
+    if (isset($_SESSION['user_id'], 
+                        $_SESSION['username'], 
+                        $_SESSION['login_string'])) {
+ 
+        $user_id = $_SESSION['user_id'];
+        $login_string = $_SESSION['login_string'];
+        $username = $_SESSION['username'];
+ 
+        // Get the user-agent string of the user.
+        $user_browser = $_SERVER['HTTP_USER_AGENT'];
+ 
+        if ($stmt = $mysqli->prepare("SELECT password 
+                                      FROM members 
+                                      WHERE id = ? LIMIT 1")) {
+            // Bind "$user_id" to parameter. 
+            $stmt->bind_param('i', $user_id);
+            $stmt->execute();   // Execute the prepared query.
+            $stmt->store_result();
+ 
+            if ($stmt->num_rows == 1) {
+                // If the user exists get variables from result.
+                $stmt->bind_result($password);
+                $stmt->fetch();
+                $login_check = hash('sha512', $password . $user_browser);
+ 
+                if ($login_check == $login_string) {
+                    // Logged In!!!! 
+                    return true;
+                } else {
+                    // Not logged in 
+                    return false;
+                }
+            } else {
+                // Not logged in 
+                return false;
+            }
+        } else {
+            // Not logged in 
+            return false;
+        }
+    } else {
+        // Not logged in 
+        return false;
+    }
 }
-}
-
 /*
 * --------------------------
 */
--- includes/process_login.php
+++ includes/process_login.php
@@ -43,20 +43,21 @@ sec_session_start();
 /*
 * 3) Login Post
 */
-	if (isset($_POST['email'], $_POST['p'])) {
-    		$email = $_POST['email'];
-    		$password = $_POST['p']; // The hashed password.
-		if (login($email, $password, $mysqli) == true) {
-       			// Login success 
-        		header('Location: ../protected_page.php');
-   		} else {
-		        // Login failed 
-        		header('Location: ../index.php?error=1');
-		}
-		} else {
-    		// The correct POST variables were not sent to this page. 
-    		echo 'Invalid Request';
-		}
+if (isset($_POST['email'], $_POST['p'])) {
+    $email = $_POST['email'];
+    $password = $_POST['p']; // The hashed password.
+ 
+    if (login($email, $password, $mysqli) == true) {
+        // Login success 
+        header('Location: ../protected_page.php');
+    } else {
+        // Login failed 
+        header('Location: ../index.php?error=1');
+    }
+} else {
+    // The correct POST variables were not sent to this page. 
+    echo 'Invalid Request';
+}
 /*
 * --------------------------
 */
