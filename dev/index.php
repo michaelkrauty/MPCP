2ac1c944714b2292fc10f54b54020deb8978d13f
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-04 08:30:04 +0000

    modified:   dev/index.php

--- dev/index.php
+++ dev/index.php
@@ -1,13 +1,19 @@
 <?php
-$data['hostname'] = NULL;
-        $data['ip'] = NULL;
-        $var = gethostname();
-        if ($var === FALSE) {
-            $var = NULL;
-        }
-        else {
-            $data['hostname'] = $var;
-            $data['ip'] = gethostbyname($var);
-        }
+
+<?php
+  $fh = fopen('/proc/meminfo');
+  $mem = 0;
+  while ($line = fgets($fh)) {
+    $pieces = array();
+    if (preg_match('^MemTotal:\s+(\d+)\skB$', $line, $pieces)) {
+      $mem = $pieces[1];
+      break;
+    }
+  }
+  fclose($fh);
+
+  echo "$mem kB RAM found"; ?>
+
+
 
  ?>
