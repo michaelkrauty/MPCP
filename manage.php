2c9f34dd362349db007017ab84e67a13ed656c69
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-03 21:19:32 -0800

    Update manage.php

--- manage.php
+++ manage.php
@@ -16,9 +16,6 @@
     <!-- Bootstrap core CSS -->
     <link href="./bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="dashboard.css" rel="stylesheet">
-
     <!-- Just for debugging purposes. Don't actually copy this line! -->
     <!--[if lt IE 9]><script src="./bootstrap/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
 
