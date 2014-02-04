2c7e31e5e7e0ed58c11bebc915ac84720fb54861
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-03 23:13:37 -0800

    Update login.php

--- login.php
+++ login.php
@@ -31,28 +31,27 @@ if (login_check($mysqli) == true) {
     <br>
     <center>
     <div class="container">
-    <div class="well">
-      <form action="includes/process_login.php" class="form-signin" role="form" method="post" name="login_form">
-      <h2 class="form-signin-heading">Sign in:</h2>
-      <?php
-        if (isset($_GET['error'])) {
-            echo '<p class="error">Error Logging In!</p>';
-        }
-        ?>
-        <input type="email" name="email" class="form-control" placeholder="Email address" required autofocus>
-        <input type="password" name="password" class="form-control" placeholder="Password" required>
-        <label class="checkbox">
-        <input type="checkbox" value="remember-me">Remember me
-        </label>
-	 <input class= "btn btn-lg btn-primary btn-block" type="button" value="Login" onclick="formhash(this.form, this.form.password);" /> 
-      </form>
-    </center>
-	<center>
-        <p>If you don't have a login, please <a href="register.php">register</a></p>
-        <p>If you are done, please <a href="includes/logout.php">log out</a>.</p>
-        <p>You are currently logged <?php echo $logged ?>.</p>
-	</center> 
-</div>
+	    <div class="well">
+	      <form action="includes/process_login.php" class="form-signin" role="form" method="post" name="login_form">
+	      <h2 class="form-signin-heading">Sign in:</h2>
+	      <?php
+	        if (isset($_GET['error'])) {
+	          echo '<p class="error">Error Logging In!</p>';
+	        }
+	      ?>
+	      <input type="email" name="email" class="form-control" placeholder="Email address" required autofocus>
+	      <input type="password" name="password" class="form-control" placeholder="Password" required>
+	      <label class="checkbox">
+	      <input type="checkbox" value="remember-me">Remember me</label>
+	      <input class= "btn btn-lg btn-primary btn-block" type="button" value="Login" onclick="formhash(this.form, this.form.password);" />
+			</form>
+	  </center>
+		<center>
+	    <p>If you don't have a login, please <a href="register.php">register</a></p>
+	    <p>If you are done, please <a href="includes/logout.php">log out</a>.</p>
+	    <p>You are currently logged <?php echo $logged ?>.</p>
+		</center> 
+	</div>
 </div>
 </body>
 </html>
