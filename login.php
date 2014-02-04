2538da0d2043e9f961fc859e694e87379ca8959a
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-04 05:01:56 +0000

    modified:   login.php

--- login.php
+++ login.php
@@ -1,40 +1,56 @@
 <?php
 include_once 'includes/db_connect.php';
 include_once 'includes/functions.php';
- 
+
 sec_session_start();
- 
+
 if (login_check($mysqli) == true) {
     $logged = 'in';
 } else {
     $logged = 'out';
 }
 ?>
+
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Secure Login: Log In</title>
-        <link rel="stylesheet" href="styles/main.css" />
-        <script type="text/JavaScript" src="js/sha512.js"></script> 
-        <script type="text/JavaScript" src="js/forms.js"></script> 
-    </head>
-    <body>
-        <?php
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+    <title>MPCP Login</title>
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="webroot/css/login.css" rel="stylesheet">
+  </head>
+  <pagetitle><center>Multiplayer Controle Panel | Login</center></pagetitle>
+  <body>
+    <br>
+    <div class="container">
+      <form action="includes/process_login.php" class="form-signin" role="form" method="post" name="login_form">
+      <center><h2 class="form-signin-heading">Sign in:</h2></center>
+      <?php
         if (isset($_GET['error'])) {
             echo '<p class="error">Error Logging In!</p>';
         }
-        ?> 
-        <form action="includes/process_login.php" method="post" name="login_form">                      
-            Email: <input type="text" name="email" />
-            Password: <input type="password" 
-                             name="password" 
-                             id="password"/>
-            <input type="button" 
-                   value="Login" 
-                   onclick="formhash(this.form, this.form.password);" /> 
-        </form>
+        ?>
+        <input type="email" name="email" class="form-control" placeholder="Email address" required autofocus>
+        <input type="password" name="password" class="form-control" placeholder="Password" required>
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"> <font color="white">Remember me</font>
+        </label>
+	 <input type="button" value="Login" onclick="formhash(this.form, this.form.password);" /> 
+      </form>
+    </div>
+	<center>
         <p>If you don't have a login, please <a href="register.php">register</a></p>
         <p>If you are done, please <a href="includes/logout.php">log out</a>.</p>
         <p>You are currently logged <?php echo $logged ?>.</p>
-    </body>
+	</center> 
+</body>
 </html>
+        <script type="text/JavaScript" src="js/sha512.js"></script> 
+        <script type="text/JavaScript" src="js/forms.js"></script> 
