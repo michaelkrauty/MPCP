db2290cd8977eb0e008c4e632e6f518e9d63f2fb
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-03 16:48:34 -0800

    Update index.php

--- querytest/index.php
+++ querytest/index.php
@@ -2,7 +2,7 @@
   include_once "../mpcp.php";
 	include_once 'MinecraftServerStatus/status.class.php';
 	$status = new MinecraftServerStatus();
-	$serverIp = "carbon.mc.gl";
+	$serverIp = "fdsfe";
 	$response = $status->getStatus($serverIp);
 	if(!$response){
 		echo "OFFLINE";
