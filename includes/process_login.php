214fd2195696b577de25046cb511bb592b6ddb73
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-04 05:03:07 +0000

    modified:   process_login.php

--- includes/process_login.php
+++ includes/process_login.php
@@ -48,7 +48,7 @@ sec_session_start();
     		$password = $_POST['p']; // The hashed password.
 		if (login($email, $password, $mysqli) == true) {
        			// Login success 
-        		header('Location: ../protected_page.php');
+        		header('Location: ../login.php');
    		} else {
 		        // Login failed 
         		header('Location: ../index.php?error=1');
