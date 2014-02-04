d66a67b61a41ca421ab1533c3a1f4b267e287b8a
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-04 07:28:07 +0000

    modified:   login.php

--- login.php
+++ login.php
@@ -32,9 +32,6 @@
 	      		<input type="checkbox" value="remember-me">Remember me</label>
 	      		<input class= "btn btn-lg btn-primary btn-block" type="button" value="Login" onclick="formhash(this.form, this.form.password);" />
 					</form>
-			    <p>If you don't have a login, please <a href="register.php">register</a></p>
-	  		  <p>If you are done, please <a href="includes/logout.php">log out</a>.</p>
-	    		<p>You are currently logged <?php echo $logged ?>.</p>
 				</div>
 			</div>
 		</center>
