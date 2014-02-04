9301f53ef5d07d2a80eb2f7ccefce49d391b08e3
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-04 04:45:25 +0000

    modified:   includes/functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -65,47 +65,50 @@ function sec_session_start(){
 /*
 * 3) Login Function
 */
-function login ($email, $password, $mysqli) {
-
-	// Prepared statment to stop SQL injection.
-	if ($stmt = $mysqli->prepare("SELECT id,  username, password, salt FROM members WHERE email = ? LIMIT 1"))
-	$stmt->bind_param('s', $email); // Bind email to the parameter
-	$stmt->execute(); // Execute out statment
-	$stmt->store_result();
-
-	// Get variables from result
-	$stmt->bind_result($user_id, $username, $dbpassword, $salt);
-	$stmt->fetch();
-
-	// Hash the password with the salt
-	$password = hash('sha512', $password . $salt);
-
-	// If account exists check if the account is locked.
-	if ($stmt->num_rows == 1) {
-		if (checkbrute($user_id, mysqli) == true) {
-		return false; // Account is locked
-		}
-	} else {
-		// Check if the password in the batabase matches & XSS protection.
-		if ($db_password == $password) {
-			$user_browser = $_SERVER['HTTP_USER_AGENT'];
-			$user_id = preg_replace("/[^0-9]+/", "", $user_id);
-			$_SESSION['user_id'] = $user_id;
-			$username = preg_replace("/[^a-zA-Z0-9_\-]+/","",$username);
-			$_SESSION['username'] = $username;
-			$_SESSION['login_string'] = hash('sha512', $password . $user_browser);
-			return true; // Login Sucsessfull
-		} else {
-			// Password is incorrect record in database.
-			 $now = time();
-			 $mysqli->query("INSERT INTO login_attempts(user_id, time) VALUES ('$user_id', '$now')");
-			return false;
-		}
-	 else {
-		return false; // No user exists.
-	}
+// hash the password with the unique salt.
+        $password = hash('sha512', $password . $salt);
+        if ($stmt->num_rows == 1) {
+            // If the user exists we check if the account is locked
+            // from too many login attempts 
+ 
+            if (checkbrute($user_id, $mysqli) == true) {
+                // Account is locked 
+                // Send an email to user saying their account is locked
+                return false;
+            } else {
+                // Check if the password in the database matches
+                // the password the user submitted.
+                if ($db_password == $password) {
+                    // Password is correct!
+                    // Get the user-agent string of the user.
+                    $user_browser = $_SERVER['HTTP_USER_AGENT'];
+                    // XSS protection as we might print this value
+                    $user_id = preg_replace("/[^0-9]+/", "", $user_id);
+                    $_SESSION['user_id'] = $user_id;
+                    // XSS protection as we might print this value
+                    $username = preg_replace("/[^a-zA-Z0-9_\-]+/", 
+                                                                "", 
+                                                                $username);
+                    $_SESSION['username'] = $username;
+                    $_SESSION['login_string'] = hash('sha512', 
+                              $password . $user_browser);
+                    // Login successful.
+                    return true;
+                } else {
+                    // Password is not correct
+                    // We record this attempt in the database
+                    $now = time();
+                    $mysqli->query("INSERT INTO login_attempts(user_id, time)
+                                    VALUES ('$user_id', '$now')");
+                    return false;
+                }
+            }
+        } else {
+            // No user exists.
+            return false;
+        }
+    }
 }
-
 /*
 * --------------------------
 */
