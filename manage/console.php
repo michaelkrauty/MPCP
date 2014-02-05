362d38d62dffeea50029f5099df05d683a2c1ae2
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-05 14:09:20 -0800

    Update console.php

--- manage/console.php
+++ manage/console.php
@@ -1,7 +1,65 @@
-<div class="wellconsole">
-                <?php
-                  include_once "consoleviewer.inc.php";
-                ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include_once "../includes/db_connect.php"; include_once "../includes/functions.php";
+      sec_session_start();
+      if(login_check($mysqli) == true){
+      ?>
+      <?php
+        include "../includes/mpcp.inc.php";
+        $serverId = "mpcp_0";
+        $logFile = "/var/mpcp/servers/" . $serverId . "/logs/latest.log";
+        $userEmail = $_SESSION['email'];
+        $userName = $_SESSION['username'];
+        $pageName = "manage";
+      ?>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="shortcut icon" href="../bootstrap/assets/ico/favicon.ico">
+      <!-- Custom styles for this template -->
+      <link href="../webroot/css/manage.css" rel="stylesheet">
+      <title>MPCP Dashboard</title>
+      <!-- Bootstrap theme -->
+      <link href='../bootstrap/dist/css/bootstrap-theme.min.css' rel='stylesheet'>
+      <!-- Bootstrap core CSS -->
+      <link href="../bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+      <!-- Just for debugging purposes. Don't actually copy this line! -->
+      <!--[if lt IE 9]><script src="./bootstrap/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+      <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+    </head>
+    <body>
+      <?php include_once "../includes/header.inc.php"; ?>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="sidebar">
+              <div class="col-sm-3 col-md-2 sidebar">
+                <div class='well'>
+                  <ul class="nav nav-sidebar">
+                    <li><a href="">Dashboard</a></li>
+                    <li><a href="">Manage</a></li>
+                    <li><a href="console.php">Console</a></li>
+                    <li><a href="">Server Jar Mod</a></li>
+                    <li><a href="">Server Configuration</a></li>
+                    <li><a href="">Server Files</a></li>
+                    <li><a href="">Scheduled Tasks</a></li>
+                    <li><a href="">Backup</a></li>
+                    <li><a href="">Plugins</a></li>
+                    <li><a href="">Support</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="wellconsole">
+              <?php
+                include_once "consoleviewer.inc.php";
+              ?>
               </div>
               <form>
               <div class="commandbar">
@@ -25,3 +83,19 @@
                 }
                 ?>
               </form>
+            </div>
+          </div>
+        </div>
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+        <script src="./bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="./bootstrap/assets/js/docs.min.js"></script>
+      </body>
+    <?php
+      }else{
+        header("Location: http://dominationvps.com/mpcp/login.php");
+      }
+  ?>
+</html>
