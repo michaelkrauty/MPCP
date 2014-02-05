11ba214a03bed247167d0f5876841c8b8fc510b1
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-05 14:24:24 -0800

    Update console.php

--- manage/console.php
+++ manage/console.php
@@ -38,24 +38,7 @@
       <?php include_once "../includes/header.inc.php"; ?>
         <div class="container-fluid">
           <div class="row">
-            <div class="sidebar">
-              <div class="col-sm-3 col-md-2 sidebar">
-                <div class='well'>
-                  <ul class="nav nav-sidebar">
-                    <li><a href="">Dashboard</a></li>
-                    <li><a href="">Manage</a></li>
-                    <li><a href="console.php">Console</a></li>
-                    <li><a href="">Server Jar Mod</a></li>
-                    <li><a href="">Server Configuration</a></li>
-                    <li><a href="">Server Files</a></li>
-                    <li><a href="">Scheduled Tasks</a></li>
-                    <li><a href="">Backup</a></li>
-                    <li><a href="">Plugins</a></li>
-                    <li><a href="">Support</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+            <?php include_once "../includes/sidebar.inc.php"; ?>
             <div class="wellconsole">
               <?php
                 include_once "consoleviewer.inc.php";
