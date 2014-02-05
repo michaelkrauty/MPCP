2f78f36ad7460c00eb34681605a12662d26df219
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-05 06:44:36 +0000

    new file:   includes/cpuusage.inc.php 	new file:   includes/ramusage.inc.php

---
+++ includes/ramusage.inc.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<div class="progress progress-info">
+  <div class="bar" style="width: 20%"></div>
+</div>
+<div class="progress progress-success">
+  <div class="bar" style="width: 40%"></div>
+</div>
+<div class="progress progress-warning">
+  <div class="bar" style="width: 60%"></div>
+</div>
+<div class="progress progress-danger">
+  <div class="bar" style="width: 80%"></div>
+</div>
