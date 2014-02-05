272b8e7a6fccc4f48c8ca18da12d533b293186b4
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-04 17:21:01 -0800

    Update consoleviewer.inc.php

--- consoleviewer.inc.php
+++ consoleviewer.inc.php
@@ -1,8 +1,9 @@
-<html>
+<!--<html>
 <head>
 <meta http-equiv="refresh" content="10">
 </head>
-<?php include "mpcp.php";
-  echo "test";
+<?php #include "mpcp.php";
+#  echo "test";
 ?>
 </html>
+-->
