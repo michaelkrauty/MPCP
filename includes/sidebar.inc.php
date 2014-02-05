c2ce9f4779490bd74e4c655150e7ae646dc09a85
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-05 14:23:42 -0800

    Create sidebar.inc.php

---
+++ includes/sidebar.inc.php
@@ -0,0 +1,17 @@
+<div class="sidebar">
+  <div class="col-sm-3 col-md-2 sidebar">
+    <div class='well'>
+      <ul class="nav nav-sidebar">
+        <li><a href="http://dominationvps.com/mpcp/manage">Dashboard</a></li>
+        <li><a href="http://dominationvps.com/mpcp/manage/console.php">Console</a></li>
+        <li><a href="http://dominationvps.com/mpcp/manage/jar.php">Server Jar Mod</a></li>
+        <li><a href="http://dominationvps.com/mpcp/manage/config.php">Server Configuration</a></li>
+        <li><a href="http://dominationvps.com/mpcp/manage/files.php">Server Files</a></li>
+        <li><a href="http://dominationvps.com/mpcp/manage/tasks.php">Scheduled Tasks</a></li>
+        <li><a href="http://dominationvps.com/mpcp/manage/backup.php">Backup</a></li>
+        <li><a href="http://dominationvps.com/mpcp/manage/plugins.php">Plugins</a></li>
+        <li><a href="http://dominationvps.com/mpcp/forum">Support</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
