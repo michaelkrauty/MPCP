f24974bb0166abf4e3b744cd4c4c4ed50464e614
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-05 04:41:03 +0000

    modified:   includes/footer.inc.php

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,4 +1,6 @@
-div class="navbar navbar-inverse navbar-fixed-bottom"
+<div class="navbar navbar-inverse navbar-fixed-bottom">
+<div class='container'>
 <p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+</div>
 </div>
 
