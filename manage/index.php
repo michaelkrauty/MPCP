2f225575ca26c0b6b2ab915e379c96758a89c856
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-05 14:24:45 -0800

    Update index.php

--- manage/index.php
+++ manage/index.php
@@ -38,23 +38,7 @@
       <?php include_once "../includes/header.inc.php"; ?>
         <div class="container-fluid">
           <div class="row">
-            <div class="sidebar">
-              <div class="col-sm-3 col-md-2 sidebar">
-                <div class='well'>
-                  <ul class="nav nav-sidebar">
-                    <li><a href="http://dominationvps.com/mpcp/manage">Dashboard</a></li>
-                    <li><a href="http://dominationvps.com/mpcp/manage/console.php">Console</a></li>
-                    <li><a href="http://dominationvps.com/mpcp/manage/jar.php">Server Jar Mod</a></li>
-                    <li><a href="http://dominationvps.com/mpcp/manage/config.php">Server Configuration</a></li>
-                    <li><a href="http://dominationvps.com/mpcp/manage/files.php">Server Files</a></li>
-                    <li><a href="http://dominationvps.com/mpcp/manage/tasks.php">Scheduled Tasks</a></li>
-                    <li><a href="http://dominationvps.com/mpcp/manage/backup.php">Backup</a></li>
-                    <li><a href="http://dominationvps.com/mpcp/manage/plugins.php">Plugins</a></li>
-                    <li><a href="http://dominationvps.com/mpcp/forum">Support</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+          <?php includes "../includes/sidebar.inc.php"; ?>
             <div class="col-sm-9 col-sm-offset-4 col-md-8 col-md-offset-0 main">
               <div class="bar1">
                 <div class="well">
