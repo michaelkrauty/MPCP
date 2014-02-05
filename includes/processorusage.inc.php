c63ff0ac5f1af4d00360e95029d2254957667181
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-05 08:31:16 +0000

    modified:   processorusage.inc.php

--- includes/processorusage.inc.php
+++ includes/processorusage.inc.php
@@ -50,7 +50,7 @@ $stat2 = GetCoreInformation();
 $data = GetCpuPercentages($stat1, $stat2);
 
 foreach( $data as $k => $v ) {
-        print_r($data['user'] . ',');
+        print_r($k['user'] . ',');
 }
 ?>
 
