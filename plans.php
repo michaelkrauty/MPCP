8068c5dbe79625a5b781bcb62da0441f12a0ccab
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-04 23:38:39 -0800

    Update plans.php

--- plans.php
+++ plans.php
@@ -1,2 +1,54 @@
-<?php
-?>
+<?php include_once "includes/db_connect.php"; include_once "includes/functions.php";
+  sec_session_start();
+  if(login_check($mysqli) == true){
+  ?>
+
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Daniel Oxenbury (http://daniel.oxituk.co.uk)">
+    <meta name="author" content="Michael Krautkramer">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+    <title>MPCP Login</title>
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="./bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="webroot/css/index.css" rel="stylesheet">
+    <?php
+      $userEmail = $_SESSION['email'];
+      $userName = $_SESSION['username'];
+    ?>
+    <?php
+      $pageName = "plans";
+    ?>
+</head>
+  <?php include_once "./includes/header.inc.php"; ?>
+  <body role='document'> <body role='document'>
+    
+  </body>
+  <?php
+    }else{
+    ?>
+    <script>
+      alert("You must be logged in to view this page!");
+    </script>
+    <?php
+      header("Location: http://dominationvps.com/mpcp/login.php");
+    }
+  ?>
+  <?php include_once "./includes/footer.inc.php"; ?>
+
+</html>
+    <!-- ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="./bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="./bootstrap/assets/js/docs.min.js"></script>
+    <script src="./webroot/js/Chart.js"></script>
