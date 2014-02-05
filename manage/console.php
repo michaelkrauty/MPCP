d4b126f6a2b922f1700f10e29613d88097a63398
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-05 14:35:21 -0800

    Update console.php

--- manage/console.php
+++ manage/console.php
@@ -39,29 +39,30 @@
         <div class="container-fluid">
           <div class="row">
             <?php include_once "../includes/sidebar.inc.php"; ?>
-            <div class="wellconsole">
-              <?php
-                include_once "consoleviewer.inc.php";
-              ?>
+            <div class="consolebox">
+              <div class="wellconsole">
+                <?php
+                  include_once "consoleviewer.inc.php";
+                ?>
               </div>
               <div class="commandbar">
-               <form>
-                <div class="col-xs-10">
-                  <input name="cmd" type="text" class="form-control" method="post" placeholder="Enter a command here...">
-                </div>
-                <div class="col-xs-2">
-                  <button class="btn btn-lg btn-primary btn-block" type="submit">Run</button>
-                </div>
-              <?php
-                if(screenOnline($serverId)){
-                  if(isset($_GET['cmd'])){
-                    $cmd = $_GET['cmd'];
-                    executeCommand($serverId, $cmd);
-                  }
-                }
-                ?>
-                </div>
-              </form>
+                <form>
+                  <div class="col-xs-10">
+                    <input name="cmd" type="text" class="form-control" method="post" placeholder="Enter a command here...">
+                  </div>
+                  <div class="col-xs-2">
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">Run</button>
+                  </div>
+                  <?php
+                    if(screenOnline($serverId)){
+                      if(isset($_GET['cmd'])){
+                        $cmd = $_GET['cmd'];
+                        executeCommand($serverId, $cmd);
+                      }
+                    }
+                  ?>
+                </form>
+              </div>
             </div>
           </div>
         </div>
