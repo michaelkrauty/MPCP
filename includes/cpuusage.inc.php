21a338a6aa38d4dbdcbe62e5054d6bd075b57192
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-05 07:12:09 +0000

    modified:   includes/cpuusage.inc.php

--- includes/cpuusage.inc.php
+++ includes/cpuusage.inc.php
@@ -1,20 +1,3 @@
-<?
-
-function sys_getloadavg_hack()
-{
-    $str = substr(strrchr(shell_exec("uptime"),":"),1);
-    $avs = array_map("trim",explode(",",$str));
-
-    return $avs;
-}
-
-print_r(sys_getloadavg_hack());
-
-// Array
-// (
-//     [0] => 6.24
-//     [1] => 4.92
-//     [2] => 3.99
-// )
-
+<?php
+echo shell_exec("cat /proc/stat");
 ?>
