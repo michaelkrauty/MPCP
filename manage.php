ea393794ce93edbfbe4cb011e4aa61d780639cef
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-04 19:22:14 -0800

    Update manage.php

--- manage.php
+++ manage.php
@@ -128,7 +128,7 @@
             <br><br>
            <?php
               if(screenOnline($serverId)){
-                if(isset($_GET['cmd'])){
+                if($_GET['cmd'] != ""){
                   $cmd = $_GET['cmd'];
                   executeCommand($serverId, $cmd);
                 }else{}
