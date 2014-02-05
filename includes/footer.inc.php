3c72be9f89bd9b83aa7c79093eba4fef79668d26
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-05 04:42:38 +0000

    modified:   includes/footer.inc.php

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,6 +1,6 @@
 <div class="navbar navbar-inverse navbar-fixed-bottom">
 <div class='container'>
-<p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+<center>CREATED BY DANIEL OXENBURY WITH NO HELP FROM MI16</center>
 </div>
 </div>
 
