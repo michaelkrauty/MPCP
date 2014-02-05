9a7b9723d44150030f18b62d547548011b024462
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-05 03:58:01 +0000

    modified:   consoleviewer.inc.php

--- consoleviewer.inc.php
+++ consoleviewer.inc.php
@@ -1,7 +1,7 @@
 <?php
-$serverId = "mpcp_0"
+$serverId = "mpcp_0";
 
-$intlol = "1";
+$ = 1;
 
 while ($i <= 100) {
 echo shell_exec("tail -n 100 /var/mpcp/servers/" . $serverId . "/logs/latest.log | sed -n $i");
