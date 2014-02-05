a236437b4ac7fac353ffa983998962820be88eb2
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-05 13:13:15 +0000

    modified:   includes/memoryusage.inc.php

--- includes/memoryusage.inc.php
+++ includes/memoryusage.inc.php
@@ -1,16 +1,18 @@
 <div class="well">
 <h2> Memory Usage </h2>
 <?php
-  $fh = fopen('/proc/meminfo');
-  $mem = 0;
-  while ($line = fgets($fh)) {
-    $pieces = array();
-    if (preg_match('^MemTotal:\s+(\d+)\skB$', $line, $pieces)) {
-      $mem = $pieces[1];
-      break;
-    }
-  }
-  fclose($fh);
 
-  echo "$mem kB RAM found"; ?>
+function getSystemMemInfo() 
+{       
+    $data = explode("\n", file_get_contents("/proc/meminfo"));
+    $meminfo = array();
+    foreach ($data as $line) {
+    	list($key, $val) = explode(":", $line);
+    	$meminfo[$key] = trim($val);
+    }
+    return $meminfo;
+}
+var_dump( getSystemMemInfo() );
+
+?>
 </div>
