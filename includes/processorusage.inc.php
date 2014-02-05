eeb3ff58eedb04996f0368807a4e31058397770f
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-05 11:39:56 +0000

    modified:   processorusage.inc.php

--- includes/processorusage.inc.php
+++ includes/processorusage.inc.php
@@ -59,7 +59,7 @@ foreach( $data as $k => $v ) {
 	echo "<br>";
 	echo "<div class=\"progress-bar progress-bar-success\" role=\"progressbar\" aria-valuenow=\" "
 	. "50" . "
-	 \"aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:\" "
+	 \"aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: "
 	 . "50%" . "
 	 \"%\">";
 	echo "<span class=\"sr-only\">40% Complete (success)</span>";
