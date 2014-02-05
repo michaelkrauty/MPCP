5430e7c2d9ce235c413cf97d661865550d85fe46
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-05 14:28:30 -0800

    Update console.php

--- manage/console.php
+++ manage/console.php
@@ -44,15 +44,14 @@
                 include_once "consoleviewer.inc.php";
               ?>
               </div>
-              <form>
               <div class="commandbar">
+               <form>
                 <div class="col-xs-10">
                   <input name="cmd" type="text" class="form-control" method="post" placeholder="Enter a command here...">
                 </div>
                 <div class="col-xs-2">
                   <button class="btn btn-lg btn-primary btn-block" type="submit">Run</button>
                 </div>
-              </div>
               <?php
                 if(screenOnline($serverId)){
                   if(isset($_GET['cmd'])){
@@ -61,6 +60,7 @@
                   }
                 }
                 ?>
+                </div>
               </form>
             </div>
           </div>
