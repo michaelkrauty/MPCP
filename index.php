2a46467f35953a917c1e54971601128282b5408b
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-05 07:40:17 +0000

    modified:   index.php

--- index.php
+++ index.php
@@ -47,59 +47,7 @@
               echo "<img src='http://signaturecraft.us/avatars/30/face/" . $userName . ".png' height='100' width='100'>";
             }
           ?><br></br>
-	<?php include_once "./includes/serverlist.inc.php"; ?>
-
-	<h2> Your Servers </h2>
-<table align="center"valign="center"class="table table-striped table-bordered table-condensed">
-	<center>	
-	<tr>
-	<th width="10%"> Server ID </th>
-	<th width="20%"> Server Address </th>
-	<th width="20%"> Server Name </th>
-	<th width="10%"> Players </th>
-	<th width="10%"> Server Status </th>
-	<th width="30%"> Quick Commands </th>
-	<tr class="success">
-		<td> 00001 </td>
-		<td> 192.168.1.1:25565 </td>
-		<td> Test Server 1 </td>
-		<td> 0 / 24 </td>
-		<td> ONLINE </td>
-		<td><form><button disabled="disabled" name="start" type="submit" class='btn btn-small btn-success'>Start</button> <button name="stop" type="submit" class='btn btn-small btn-danger'>Stop</button> <button name="restart" type="submit" class='btn btn-small btn-warning'>Restart</button> <button name="reload" type="submit" class='btn btn-small btn-info'>Reload</button></form></td>
-	</tr>
-	<tr class="success">
-		<td> 00002 </td>
-		<td> 192.168.1.2:25565 </td>
-		<td> Test Server 2 </td>
-		<td> 0 / 24 </td>
-		<td> ONLINE </td>
-		<td><form><button disabled="disabled" name="start" type="submit" class='btn btn-small btn-success'>Start</button> <button name="stop" type="submit" class='btn btn-small btn-danger'>Stop</button> <button name="restart" type="submit" class='btn btn-small btn-warning'>Restart</button> <button name="reload" type="submit" class='btn btn-small btn-info'>Reload</button></form></td>
-	<tr class="warning">
-		<td> 00003 </td>
-		<td> 192.168.1.3:25565 </td>
-		<td> Test Server 3 </td>
-		<td> 0 / 24 </td>
-		<td> OFFLINE </td>
-		<td><form><button name="start" type="submit" class='btn btn-small btn-success'>Start</button> <button disabled="disabled" name="stop" type="submit" class='btn btn-small btn-danger'>Stop</button> <button disabled="disabled" name="restart" type="submit" class='btn btn-small btn-warning'>Restart</button> <button disabled="disabled" name="reload" type="submit" class='btn btn-small btn-info'>Reload</button></form></td>
-		</tr>
-	<tr class="success">
-		<td> 00004 </td>
-		<td> 192.168.1.4:25565 </td>
-		<td> Test Server 4 </td>
-		<td> 0 / 24 </td>
-		<td> ONLINE </td>
-		<td><form><button disabled="disabled" name="start" type="submit" class='btn btn-small btn-success'>Start</button> <button name="stop" type="submit" class='btn btn-small btn-danger'>Stop</button> <button name="restart" type="submit" class='btn btn-small btn-warning'>Restart</button> <button name="reload" type="submit" class='btn btn-small btn-info'>Reload</button></form></td>
-	</tr>
-	<tr class="warning">
-		<td> 00005 </td>
-		<td> 192.168.1.5:25565 </td>
-		<td> Test Server 5 </td>
-		<td> 0 / 24 </td>
-		<td> OFFLINE </td>
-		<td><form><button name="start" type="submit" class='btn btn-small btn-success'>Start</button> <button disabled="disabled" name="stop" type="submit" class='btn btn-small btn-danger'>Stop</button> <button disabled="disabled" name="restart" type="submit" class='btn btn-small btn-warning'>Restart</button> <button disabled="disabled" name="reload" type="submit" class='btn btn-small btn-info'>Reload</button></form></td>
-	</tr>
-	</center>
-</table>
+	  <?php include_once "./includes/serverlist.inc.php"; ?>
 	<h2> CPU Usage </h2>
 	
 
