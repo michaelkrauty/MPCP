91565d60653c20242aab4a121aa86da43d776b35
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-04 19:33:20 -0800

    Update consoleviewer.inc.php

--- consoleviewer.inc.php
+++ consoleviewer.inc.php
@@ -1,5 +1,5 @@
 <?php
   $cmd = shell_exec("tail -n 100 /var/mpcp/servers/" . $serverId . "/logs/latest.log");
-  $out = str_replace("\r","\n",$cmd);
+  $out = str_replace("\\r","\\n",$cmd);
   echo $out;
 ?>
