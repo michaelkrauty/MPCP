0101f4f51a5c2797d965f47ae5aae97f09c53c63
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-05 12:09:15 +0000

    modified:   processorusage.inc.php

--- includes/processorusage.inc.php
+++ includes/processorusage.inc.php
@@ -56,8 +56,8 @@ foreach( $data as $k => $v ) {
 	$i = 20;
 	if ($i > 1) {
 	echo "<div class=\"progress progress-striped\">";
-  	echo "<div class=\"progress-bar progress-bar-success\" role=\"progressbar\" aria-valuenow=\"20\"
- 	aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:20%\">";
+  	echo "<div class=\"progress-bar progress-bar-success\" role=\"progressbar\" aria-valuenow=\"${$i}\"
+ 	aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:${$i}%\">";
     	/*echo '<span class=\"sr-only\">40% Complete (success)</span>';*/
   	echo "</div>";
 	echo "</div>";
