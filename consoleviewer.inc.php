75c730a1877f5a4a96f64a9bbd6898ec4a58ab9b
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-05 03:53:49 +0000

    modified:   consoleviewer.inc.php

--- consoleviewer.inc.php
+++ consoleviewer.inc.php
@@ -1,9 +1,8 @@
 <?php
 $i = 1;
 while ($i <= 100) {
-$line = shell_exec("tail -n 100 /var/mpcp/servers/" . $serverId . "/logs/latest.log | sed -n $i");
-$line .= "\r\n";
-echo $line;
+echo shell_exec("tail -n 100 /var/mpcp/servers/" . $serverId . "/logs/latest.log | sed -n $i");
+echo "\r\n";
 $i++;
 }
 ?>
