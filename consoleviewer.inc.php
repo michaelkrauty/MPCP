ed7bee32a38b483281ed060c6a5205a9c5309dac
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-04 19:29:03 -0800

    Update consoleviewer.inc.php

--- consoleviewer.inc.php
+++ consoleviewer.inc.php
@@ -1,3 +1,5 @@
 <?php
-  echo shell_exec("tail -n 100 /var/mpcp/servers/" . $serverId . "/logs/latest.log");
+#  $out = shell_exec("tail -n 100 /var/mpcp/servers/" . $serverId . "/logs/latest.log");
+  $file = "/var/mpcp/servers/" . $serverId . "/logs/latest.log";
+  $line = `sed -e "s/\\r/\\n/g" $file | tail -n 100`
 ?>
