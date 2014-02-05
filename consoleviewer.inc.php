8a58285bebf12388003bd7b08e73da887ab76824
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-04 20:33:27 -0800

    Update consoleviewer.inc.php

--- consoleviewer.inc.php
+++ consoleviewer.inc.php
@@ -1,13 +1,16 @@
 <?php
-  
-  $i = 1;
-  
-  while ($i <= 100) {
-    if(shell_exec("tail -n 100 /var/mpcp/servers/" . $serverId . "/logs/latest.log | sed -n '$i p'") != ""){
-      echo shell_exec("tail -n 100 /var/mpcp/servers/" . $serverId . "/logs/latest.log | sed -n '$i p'");
-      echo "<br>";
+  $x=1; 
+  while($x<=5){
+    $i = 1;
+    while ($i <= 100) {
+      if(shell_exec("tail -n 100 /var/mpcp/servers/" . $serverId . "/logs/latest.log | sed -n '$i p'") != ""){
+        echo shell_exec("tail -n 100 /var/mpcp/servers/" . $serverId . "/logs/latest.log | sed -n '$i p'");
+        echo "<br>";
+      }
+      $i++;
     }
-  $i++;
+    sleep(5);
+    $x++;
   }
   echo "<br>";
 ?>
