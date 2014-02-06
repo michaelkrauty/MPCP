15b09271a15463f3c2348a3962b9615b060c412e
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-06 15:21:50 +0000

    modified:   includes/fileexplorer.inc.php

--- includes/fileexplorer.inc.php
+++ includes/fileexplorer.inc.php
@@ -42,17 +42,17 @@
 $serverId = "mpcp_0";
 
 if ($handle = opendir('/var/mpcp/servers/' . $serverId)) {
-    echo "Directory handle: $handle\n";
-    echo "Entries:\n";
 
     /* This is the correct way to loop over the directory. */
     while (false !== ($entry = readdir($handle))) {
         echo "$entry\n";
+	echo "<br>";
     }
 
     /* This is the WRONG way to loop over the directory. */
     while ($entry = readdir($handle)) {
         echo "$entry\n";
+	echo "<br>";
     }
 
     closedir($handle);
