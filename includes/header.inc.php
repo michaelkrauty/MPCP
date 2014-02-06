e69795956c6c9db6546a232afcd25b6864402f36
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-06 12:25:51 -0800

    Update header.inc.php

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -9,13 +9,13 @@
     </div>
     <div class='navbar-collapse'>
       <ul class='nav navbar-nav'>
-	<?php if($pageName == "index"){echo "<li class='active'>";}else{echo "<li>";}?><a href='__dir/mpcp'><span class="glyphicon glyphicon-home" style="font-size:28px;"></span></a></li>
-        <?php if($pageName == "manage"){echo "<li class='active'>";}else{echo "<li>";}?><a href='__dir/mpcp/manage'><span class="glyphicon glyphicon-cog" style="font-size:28px;"></span><br></a></li>
-        <?php if($pageName == "plans"){echo "<li class='active'>";}else{echo "<li>";}?><a href='__dir/mpcp/plans.php'><span class="glyphicon glyphicon-tag" style="font-size:28px;"></span><br></a></li>
+	<?php if($pageName == "index"){echo "<li class='active'>";}else{echo "<li>";}?><a href='_dir/mpcp'><span class="glyphicon glyphicon-home" style="font-size:28px;"></span></a></li>
+        <?php if($pageName == "manage"){echo "<li class='active'>";}else{echo "<li>";}?><a href='_dir/mpcp/manage'><span class="glyphicon glyphicon-cog" style="font-size:28px;"></span><br></a></li>
+        <?php if($pageName == "plans"){echo "<li class='active'>";}else{echo "<li>";}?><a href='_dir/mpcp/plans.php'><span class="glyphicon glyphicon-tag" style="font-size:28px;"></span><br></a></li>
      </ul>
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
-          <a href="" class="dropdown-toggle" data-toggle="dropdown"></span> <?php include_once "__dir/mpcp/includes/minecrafticonsmall.inc.php" ?><?php echo " " . $userName; ?> <b class="caret"></b></a>
+          <a href="" class="dropdown-toggle" data-toggle="dropdown"></span> <?php include_once "_dir/mpcp/includes/minecrafticonsmall.inc.php" ?><?php echo " " . $userName; ?> <b class="caret"></b></a>
           <ul class="dropdown-menu">
             <li><a href="http://dominationvps.com/mpcp/settings/"><span class="glyphicon glyphicon-dashboard"></span> Settings</a></li>
             <li><a href=""><span class="glyphicon glyphicon-plus-sign"></span> Sub Users</a></li>
