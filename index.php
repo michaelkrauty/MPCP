d7de2c3396606bec4167602a18a0899a1000371d
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-06 11:52:41 +0000

    modified:   index.php

--- index.php
+++ index.php
@@ -44,6 +44,16 @@
 	</center>
       </div>
     </div>
+<div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+    <span class="sr-only">60% Complete</span>
+  </div>
+<div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+    <span class="sr-only">60% Complete</span>
+  </div>
+</div>
+</div>
   </body>
   <?php
     }else{
