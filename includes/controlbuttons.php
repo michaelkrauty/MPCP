86e4aba1fe32f2a0e9c4b0d6df62f2fb7a855338
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-06 07:05:16 +0000

    deleted:    controlbuttons.php 	deleted:    cpuusage.inc.php

--- includes/controlbuttons.php
+++
@@ -1,8 +0,0 @@
-<div class="btn-toolbar">
-  <div class="btn-group">
-    <a class="btn" href="#"><i class="icon-align-left"></i></a>
-    <a class="btn" href="#"><i class="icon-align-center"></i></a>
-    <a class="btn" href="#"><i class="icon-align-right"></i></a>
-    <a class="btn" href="#"><i class="icon-align-justify"></i></a>
-  </div>
-</div>
