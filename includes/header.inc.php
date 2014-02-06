c29d253e3860a37ac09fc97386f52425cecb0a0c
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-06 10:47:56 +0000

    modified:   header.inc.php

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -9,9 +9,9 @@
     </div>
     <div class='navbar-collapse'>
       <ul class='nav navbar-nav'>
-	<?php if($pageName == "index"){echo "<li class='active'>";}else{echo "<li>";}?><a href='http://dominationvps.com/mpcp'><span class="glyphicon glyphicon-home" style="font-size:20px;"></span><br> Home</a></li>
-        <?php if($pageName == "manage"){echo "<li class='active'>";}else{echo "<li>";}?><a href='http://dominationvps.com/mpcp/manage'><span class="glyphicon glyphicon-cog" style="font-size:20px;"></span><br> Manage</a></li>
-        <?php if($pageName == "plans"){echo "<li class='active'>";}else{echo "<li>";}?><a href='http://dominationvps.com/mpcp/plans.php'><span class="glyphicon glyphicon-tag" style="font-size:20px;"></span><br> Plans</a></li>
+	<?php if($pageName == "index"){echo "<li class='active'>";}else{echo "<li>";}?><center><a href='http://dominationvps.com/mpcp'><span class="glyphicon glyphicon-home" style="font-size:20px;"></span><br> Home</a></center></li>
+        <?php if($pageName == "manage"){echo "<li class='active'>";}else{echo "<li>";}?><center><a href='http://dominationvps.com/mpcp/manage'><span class="glyphicon glyphicon-cog" style="font-size:20px;"></span><br> Manage</a></center></li>
+        <?php if($pageName == "plans"){echo "<li class='active'>";}else{echo "<li>";}?><center><a href='http://dominationvps.com/mpcp/plans.php'><span class="glyphicon glyphicon-tag" style="font-size:20px;"></span><br> Plans</a></center></li>
      </ul>
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
