4b43f18503f27add97f64a4f3d4fb7ff327f2d91
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-06 15:55:48 -0800

    Update manage.php

--- manage.php
+++ manage.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <?php include_once "../includes/db_connect.php"; include_once "../includes/functions.php";
+    <?php include_once "includes/db_connect.php"; include_once "../includes/functions.php";
       sec_session_start();
       if(login_check($mysqli) == true){
       ?>
       <?php
-        include "../includes/mpcp.inc.php";
+        include "includes/mpcp.inc.php";
         $serverId = "mpcp_0";
         $logFile = "/var/mpcp/servers/" . $serverId . "/logs/latest.log";
         $userEmail = $_SESSION['email'];
@@ -18,14 +18,14 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="description" content="">
       <meta name="author" content="">
-      <link rel="shortcut icon" href="../bootstrap/assets/ico/favicon.ico">
+      <link rel="shortcut icon" href="bootstrap/assets/ico/favicon.ico">
       <!-- Custom styles for this template -->
-      <link href="../webroot/css/manage.css" rel="stylesheet">
+      <link href="webroot/css/manage.css" rel="stylesheet">
       <title>MPCP Dashboard</title>
       <!-- Bootstrap theme -->
-      <link href='../bootstrap/dist/css/bootstrap-theme.min.css' rel='stylesheet'>
+      <link href='bootstrap/dist/css/bootstrap-theme.min.css' rel='stylesheet'>
       <!-- Bootstrap core CSS -->
-      <link href="../bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+      <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
       <!-- Just for debugging purposes. Don't actually copy this line! -->
       <!--[if lt IE 9]><script src="./bootstrap/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
       <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -35,10 +35,10 @@
       <![endif]-->
     </head>
     <body>
-      <?php include_once "../includes/header.inc.php"; ?>
+      <?php include_once "includes/header.inc.php"; ?>
         <div class="container-fluid">
           <div class="row">
-          <?php include_once "../includes/sidebar.inc.php"; ?>
+          <?php include_once "includes/sidebar.inc.php"; ?>
             <div class="col-sm-9 col-sm-offset-4 col-md-8 col-md-offset-0 main">
               <div class="bar1">
                 <div class="well">
@@ -79,7 +79,7 @@
         <script src="./bootstrap/assets/js/docs.min.js"></script>
         <script type="text/javascript">
           var auto_refresh = setInterval( function () {
-          $('#status').load('../includes/status.inc.php');
+          $('#status').load('includes/status.inc.php');
           }, 1000);
         </script>
       </body>
