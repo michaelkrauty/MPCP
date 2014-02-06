91d263528561d0b3f910865526bdd399499c2bdf
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-06 09:59:20 +0000

    modified:   includes/header.inc.php

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -9,10 +9,11 @@
     </div>
     <div class='navbar-collapse'>
       <ul class='nav navbar-nav'>
-        <?php if($pageName == "index"){echo "<li class='active'>";}else{echo "<li>";}?><a href='http://dominationvps.com/mpcp'><span class="glyphicon glyphicon-home"></span><br> Home</a></li>
+	<center>
+        <?php if($pageName == "index"){echo "<li class='active'>";}else{echo "<li>";}?><a href='http://dominationvps.com/mpcp'><span class="glyphicon glyphicon-home-large"></span><br> Home</a></li>
         <?php if($pageName == "manage"){echo "<li class='active'>";}else{echo "<li>";}?><a href='http://dominationvps.com/mpcp/manage'><span class="glyphicon glyphicon-cog"></span><br> Manage</a></li>
         <?php if($pageName == "plans"){echo "<li class='active'>";}else{echo "<li>";}?><a href='http://dominationvps.com/mpcp/plans.php'><span class="glyphicon glyphicon-tag"></span><br> Plans</a></li>
-        
+        </center>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
