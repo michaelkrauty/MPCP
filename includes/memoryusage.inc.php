1f47717977e032eedeab8053a6c873306e5b44b1
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-06 07:24:01 +0000

    modified:   memoryusage.inc.php

--- includes/memoryusage.inc.php
+++ includes/memoryusage.inc.php
@@ -15,13 +15,11 @@
     $memfree = substr($meminfo["MemFree"], 0, -3);
     $memtotal = substr($meminfo["MemTotal"], 0, -3);
     echo "<br>";
-    echo "Memory Total : " . intval($memtotal * 0.000976563) . "MB";
+    echo "Memory Total : " . intval($memtotal * 0.000976563) . " MB";
     echo "<br>";
     echo "Used : " . intval($memused * 0.000976563) . " MB";
     echo "<br>";
     echo "Free : " . intval($memfree * 0.000976563) . " MB";
-    echo "<br>";
-    echo "Memory Total : " . intval($memtotal * 0.000976563) . " MB";
 
 ?>
 
