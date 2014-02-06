0cf9b373bf53ea3cd2140ba14863d70f5d648a28
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-06 09:51:12 +0000

    modified:   header.inc.php 	new file:   minecrafticonsmall.inc.php

---
+++ includes/minecrafticonsmall.inc.php
@@ -0,0 +1,11 @@
+<?php
+          $image1 = "http://signaturecraft.us/avatars/30/face/" . $userName . ".png";
+          $image2 = "http://signaturecraft.us/avatars/30/face/noskin.png";
+            $md5image1 = md5(file_get_contents($image1));
+            $md5image2 = md5(file_get_contents($image2));
+            if($md5image1 == $md5image2){
+              echo "<img src='http://signaturecraft.us/avatars/30/face/Herobrine.png' height='32' width='32'>";
+            }else{
+              echo "<img src='http://signaturecraft.us/avatars/30/face/" . $userName . ".png' height='32' width='32'>";
+            }
+          ?>
