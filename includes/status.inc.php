e60f53517bf5711dbb1c255c807a10c8ab1bbb91
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-05 22:46:06 -0800

    Update status.inc.php

--- includes/status.inc.php
+++ includes/status.inc.php
@@ -1,18 +1,20 @@
 <?php include "mpcp.inc.php";
   $serverId = "mpcp_0";
   if(screenOnline($serverId)){
-    echo '
-    <div class="alert alert-success">
-      <center>
-        <strong>Server Status: Server is Online!</strong>
-      </center>
-    </div>';
-  }else{
-    echo '
-    <div class="alert alert-danger">
-      <center>
-        <strong>Server Status: Server is Offline!</strong>
-      </center>
-    </div>';
+    if(serverOnline($serverId)){
+      echo '
+      <div class="alert alert-success">
+        <center>
+          <strong>Server Status: Server is Online!</strong>
+        </center>
+      </div>';
+    }else{
+      echo '
+      <div class="alert alert-danger">
+        <center>
+          <strong>Server Status: Server is Offline!</strong>
+        </center>
+      </div>';
+    }
   }
 ?>
