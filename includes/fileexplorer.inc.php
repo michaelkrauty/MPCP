ee361ef4c8d3abd28506b1de0ece990a058cf455
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-06 13:36:36 +0000

    modified:   includes/fileexplorer.inc.php

--- includes/fileexplorer.inc.php
+++ includes/fileexplorer.inc.php
@@ -1,23 +1,23 @@
 <div class="well">
 <h2> Memory Usage </h2>
-<div class="actions button-container">
-                  <div class="button-group">
-                      <a id="filebrowserhomebtn" class="button icon home">Home Directory</a>
-                      <a id="filebrowserrefreshbtn" class="button icon loop">Refresh</a>
-                  </div>
-                  <div class="button-group">
-                      <a id="filebrowsernewdirectorybtn" class="button">New Directory</a>
-                      <a id="filebrowsernewfilebtn" class="button">New File</a>
-                      <a id="filebrowseruploadfilebtn" class="button icon arrowup">Upload File</a>
-                  </div>
-                  <div class="button-group">
-                      <a id="filebrowserzipbtn" class="button">Zip</a>
-                      <a id="filebrowserunzipbtn" class="button disabled">UnZip</a>
-                  </div>
-                  <div class="button-group">
-                      <a id="filebrowsereditfilebtn" class="button icon edit disabled">Edit File</a>
-                      <a id="filebrowserdownloadbtn" class="button icon arrowdown">Download</a>
-                      <a id="filebrowserrenamebtn" class="button disabled">Rename</a>
-                      <a id="filebrowserdeletebtn" class="button icon trash disabled">Delete</a>
-                  </div>
-                </div>
+<div class="btn-group">
+<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-home"></span></button>
+<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-refresh"></span></button>
+<button type="button" class="btn btn-default">New Folder</button>
+</div>
+<div class="btn-group">
+<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-plus"></span></button>
+<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-wrench"></span></button>
+<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-font"></span></button>
+<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-trash"></span></button>
+</div>
+<div class="btn-group">
+<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-open"></span></button>
+<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-save"></span></button>
+</div>
+<div class="btn-group">
+<button type="button" class="btn btn-default"></span> Zip</button>
+<button type="button" class="btn btn-default"></span> Unzip</button>
+</div>
+</div>
