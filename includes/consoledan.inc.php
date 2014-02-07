2470d341e641e0af4330455f26e4c8e9414c14aa
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-07 11:49:36 +0000

    modified:   includes/consoledan.inc.php

--- includes/consoledan.inc.php
+++ includes/consoledan.inc.php
@@ -2,9 +2,9 @@
 <h2> Console </h2>
 <?php
 $serverId = "mpcp_0";
-$logFile = "/var/mpcp/servers/" . $serverId . "/logs/latest.log";
+$logfile = "/var/mpcp/servers/" . $serverId . "/logs/latest.log";
 echo "<div style=\"background-color:black; color:white; text-align:left; padding:10px; height:400px; overflow:scroll\">";
-$file = fopen($logfile, "r");
+$file = fopen("$logfile", "r");
 while (($line = fgets($file)) !== false) {
         echo "$line\n";
 }
