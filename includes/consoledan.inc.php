8fe2329f76b680a939a18bd36cf9698f86af0fae
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-07 11:45:13 +0000

    modified:   includes/consoledan.inc.php

--- includes/consoledan.inc.php
+++ includes/consoledan.inc.php
@@ -4,7 +4,11 @@
 $serverId = "mpcp_0";
 $logFile = "/var/mpcp/servers/" . $serverId . "/logs/latest.log";
 echo "<div style=\"background-color:black; color:white; text-align:left; padding:10px; height:400px; overflow:scroll\">";
-echo readfile($logFile);
+$file = fopen(logfile);
+while (($line = fgets($file)) !== false) {
+        echo $line\n;
+}
+fclose(logfile);
 echo "</div>";
 
 ?>
