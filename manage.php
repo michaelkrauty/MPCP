b63d40503102b4872e2d244d205940eef9148dc9
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-06 16:00:08 -0800

    Update manage.php

--- manage.php
+++ manage.php
@@ -75,8 +75,8 @@
         ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
         <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-        <script src="./bootstrap/dist/js/bootstrap.min.js"></script>
-        <script src="./bootstrap/assets/js/docs.min.js"></script>
+        <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="bootstrap/assets/js/docs.min.js"></script>
         <script type="text/javascript">
           var auto_refresh = setInterval( function () {
           $('#status').load('includes/status.inc.php');
