286b3e5467a1c65d39b4b67349e70f9755cde651
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-07 11:18:14 +0000

    modified:   includes/consoledan.inc.php

--- includes/consoledan.inc.php
+++ includes/consoledan.inc.php
@@ -1,7 +1,7 @@
 <div class="well">
 <h2> Console </h2>
 <?php
-echo "<div style=\"background-color:black; color:white; text-align:left\">";
+echo "<div style=\"background-color:black; color:white; text-align:left; padding:10px\">";
 echo "test console text";
 echo "</div>";
 ?>
