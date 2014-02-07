769d801a3ac6a00526b8b01f9a3c432dd4a9a686
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-07 13:43:22 +0000

    modified:   includes/systeminformation.inc.php

--- includes/systeminformation.inc.php
+++ includes/systeminformation.inc.php
@@ -44,6 +44,12 @@ return @date('h:i:s A');
 }
 /** Gets the servers uptime **/
 function getuptime() {
+$uptime = shell_exec("cut -d. -f1 /proc/uptime");
+$days = floor($uptime/60/60/24);
+$hours = $uptime/60/60%24;
+$mins = $uptime/60%60;
+$secs = $uptime%60;
+return "up $days days $hours hours $mins minutes and $secs seconds";
 }
 
 echo "<br>";
