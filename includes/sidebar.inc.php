bb7f17d45ae70719a88e2e98b6957bc97503484d
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-06 16:02:11 -0800

    Update sidebar.inc.php

--- includes/sidebar.inc.php
+++ includes/sidebar.inc.php
@@ -3,13 +3,13 @@
     <div class='well'>
       <ul class="nav nav-sidebar">
         <li><a href="http://dominationvps.com/mpcp/manage.php">Dashboard</a></li>
-        <li><a href="http://dominationvps.com/mpcp/manage.php?console">Console</a></li>
-        <li><a href="http://dominationvps.com/mpcp/manage.php?jar">Server Jar Mod</a></li>
-        <li><a href="http://dominationvps.com/mpcp/manage.php?config">Server Configuration</a></li>
-        <li><a href="http://dominationvps.com/mpcp/manage.php?files">Server Files</a></li>
-        <li><a href="http://dominationvps.com/mpcp/manage.php?tasks">Scheduled Tasks</a></li>
-        <li><a href="http://dominationvps.com/mpcp/manage.php?backup">Backup</a></li>
-        <li><a href="http://dominationvps.com/mpcp/manage.php?plugins">Plugins</a></li>
+        <li><a href="http://dominationvps.com/mpcp/manage.php?p=console">Console</a></li>
+        <li><a href="http://dominationvps.com/mpcp/manage.php?p=jar">Server Jar Mod</a></li>
+        <li><a href="http://dominationvps.com/mpcp/manage.php?p=config">Server Configuration</a></li>
+        <li><a href="http://dominationvps.com/mpcp/manage.php?p=files">Server Files</a></li>
+        <li><a href="http://dominationvps.com/mpcp/manage.php?p=tasks">Scheduled Tasks</a></li>
+        <li><a href="http://dominationvps.com/mpcp/manage.php?p=backup">Backup</a></li>
+        <li><a href="http://dominationvps.com/mpcp/manage.php?p=plugins">Plugins</a></li>
         <li><a href="http://dominationvps.com/mpcp/forum.php">Support</a></li>
       </ul>
     </div>
