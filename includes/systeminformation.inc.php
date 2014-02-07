3baa56456e72fb5d59329f8c53ac1337a81efc0b
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-07 13:04:36 +0000

    modified:   includes/systeminformation.inc.php

--- includes/systeminformation.inc.php
+++ includes/systeminformation.inc.php
@@ -1,40 +1,38 @@
 <div class="well">
 <h2> System Information </h2>
 <?php
-
-
 /** Gets the servers operating system **/
-function getos {
+function getos() {
 }
 /** Gets the servers distrobution**/
-function getdistro {
+function getdistro() {
 }
 /** Gets the servers architecture**/
-function getarc {
+function getarc() {
 }
 /** Gets the servers web server version**/
-function getwebsvr {
+function getwebsvr() {
 }
 /** Gets the servers WAN address **/
-function getwan {
+function getwan() {
 }
 /** Gets the servers php version **/
-function getphp {
+function getphp() {
 }
 /** Gets the servers java version **/
-function getjava {
+function getjava() {
 }
 /** Gets the current date**/
-function getdate {
+function getdate() {
 }
 /** Gets the current time**/
-function gettime {
+function gettime() {
 }
 /** Gets the servers uptime **/
-function getuptime {
+function getuptime() {
 }
 
 echo php_uname();
 echo PHP_OS;
-}
+
 ?>
