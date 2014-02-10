8de7f3a6608ba2c86c0132afe611d0ff9fa30a1e
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-10 15:12:07 -0800

    Update manage.php

--- manage.php
+++ manage.php
@@ -44,7 +44,30 @@
                 <div class="well">
                   <h2>Server Address: <strong>VPS's IP address goes here</strong></h2>
                   <div class='progress-bar' role='progressbar' aria-valuenow='19' aria-valuemin='0' aria-valuemax='20' style='width: 90%;'><span class='sr-only'></span></div>
-              <div id="buttons"></div>
+                  <div id="buttons">
+                  <?php include "mpcp.inc.php";
+                    if(count($_POST) > 0 && isset($_POST['start'])) {
+                      $memory = 1024;
+                      $jar = "craftbukkit-1.7.2-R0.3-20131225.043511-4.jar";
+                      startServer($serverId, $memory, $jar);
+                    }
+                    if(count($_POST) > 0 && isset($_POST['stop'])) {
+                      stopServer($serverId);
+                    }
+                    if(count($_POST) > 0 && isset($_POST['restart'])) {
+                      restartServer($serverId);
+                    }
+                    if(count($_POST) > 0 && isset($_POST['reload'])) {
+                      reloadServer($serverId);
+                    }
+                  ?>
+                  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                  <button name="start" type="submit" class='btn btn-lg btn-success' <?php if(screenOnline($serverId)){echo "disabled='disabled'";}?>>Start</button>
+                  <button name="stop" type="submit" class='btn btn-lg btn-danger' <?php if(!screenOnline($serverId)){echo "disabled='disabled'";}?>>Stop</button>
+                  <button name="restart" type="submit" class='btn btn-lg btn-warning' <?php if(!screenOnline($serverId)){echo "disabled='disabled'";}?>>Restart</button>
+                  <button name="reload" type="submit" class='btn btn-lg btn-info' <?php if(!screenOnline($serverId)){echo "disabled='disabled'";}?>>Reload</button>
+                  </form>
+                  </div>
                 </div>
               </div>
               <div class="status-header" id="status"></div>
