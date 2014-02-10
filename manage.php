281a541dbfaf44d948edf4c13cb27cb2d04bed81
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-10 15:16:14 -0800

    Update manage.php

--- manage.php
+++ manage.php
@@ -81,7 +81,6 @@
         <script src="bootstrap/assets/js/docs.min.js"></script>
         <script type="text/javascript">
           var auto_refresh = setInterval( function () {
-          $('#buttons').reload();
           $('#status').load('includes/status.inc.php');
           }, 1000);
         </script>
