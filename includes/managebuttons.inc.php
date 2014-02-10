e064ee3e5b7316dee9e5ab381a3ed735c9ae6610
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-10 15:00:33 -0800

    Update managebuttons.inc.php

--- includes/managebuttons.inc.php
+++ includes/managebuttons.inc.php
@@ -1,16 +1,1 @@
-<?php
-  if(count($_POST) > 0 && isset($_POST['start'])) {
-    $memory = 1024;
-    $jar = "craftbukkit-1.7.2-R0.3-20131225.043511-4.jar";
-    startServer($serverId, $memory, $jar);
-  }
-  if(count($_POST) > 0 && isset($_POST['stop'])) {
-    stopServer($serverId);
-  }
-  if(count($_POST) > 0 && isset($_POST['restart'])) {
-    restartServer($serverId);
-  }
-  if(count($_POST) > 0 && isset($_POST['reload'])) {
-    reloadServer($serverId);
-  }
-?>
+<?php?>
