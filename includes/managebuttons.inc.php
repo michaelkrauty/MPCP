22e942eff3d93be973dd7fca1e95b10dc11776cd
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-10 15:11:12 -0800

    Update managebuttons.inc.php

--- includes/managebuttons.inc.php
+++ includes/managebuttons.inc.php
@@ -1,4 +1,4 @@
-                  <?php include "mpcp.inc.php"; include "../manage.php";
+                  <?php include "mpcp.inc.php";
                     if(count($_POST) > 0 && isset($_POST['start'])) {
                       $memory = 1024;
                       $jar = "craftbukkit-1.7.2-R0.3-20131225.043511-4.jar";
