9f74a5ba9c01a26072fa84d1faa87162cec36776
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-10 12:51:03 -0800

    Update mpcp.inc.php

--- includes/mpcp.inc.php
+++ includes/mpcp.inc.php
@@ -35,7 +35,7 @@
   function startServer($serverId, $memory, $jar){
     if(!screenOnline($serverId)){
     	if(!serverOnline($serverId)){
-      	shell_exec("cd /var/mpcp/servers/" . $serverId " && screen -dmS " . $serverId . " java -Xmx" . $memory . "M -jar /var/mpcp/jar/" . $jar);
+      	shell_exec("cd /var/mpcp/servers/" . $serverId . " && screen -dmS " . $serverId . " java -Xmx" . $memory . "M -jar /var/mpcp/jar/" . $jar);
     	}
     }
   }
