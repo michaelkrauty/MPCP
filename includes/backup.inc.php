2f301562b3e8c59caaba302c3887b20e922da76e
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-11 11:05:41 -0800

    Update backup.inc.php

--- includes/backup.inc.php
+++ includes/backup.inc.php
@@ -1,2 +1,7 @@
-<?php
-?>
+                  <?php
+                    if(count($_POST) > 0 && isset($_POST['backup'])) {
+                      backupServer($serverId);
+                    }
+                  ?>
+                  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>"><button name="backup" type="submit" class='btn btn-lg btn-normal'>Backup</button>
+                  </form>
