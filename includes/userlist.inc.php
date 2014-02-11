8d47a9e185fdeee7e1696c888238c5fa9d8c6a6e
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-10 22:34:22 -0800

    Update userlist.inc.php

--- includes/userlist.inc.php
+++ includes/userlist.inc.php
@@ -24,7 +24,7 @@ while($row = mysqli_fetch_array($result))
   echo "<tr>";
   echo "<td>" . $row['userid'] . "</td>";
   echo "<td>" . MCIcon($row['mcusername']) . "</td>";
-  echo "<img src='http://signaturecraft.us/avatars/30/face/Herobrine.png' height='32' width='32'>";
+  echo "<td><img src='http://signaturecraft.us/avatars/30/face/Herobrine.png' height='32' width='32'></td>";
   echo "<td>" . $row['email'] . "</td>";
   echo "<td>" . $row['password'] . "</td>";
   echo "<td>" . $row['mcusername'] . "</td>";
