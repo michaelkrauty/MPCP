58be88fea38bb55984f650a12ca84e6b82a4ef70
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-11 03:50:43 +0000

    modified:   includes/createplans.inc.php

--- includes/createplans.inc.php
+++ includes/createplans.inc.php
@@ -1,31 +1,24 @@
 	<?php
 		include_once 'db_connect.php';
 		$error_msg = "";
-		$salt = "bFUt4WUp";
 
-		// $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-		// $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-		// $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-		// $random_salt = hash('sha512', uniqid(openssl_random_pseudo_bytes(16), TRUE));
-		// $password = hash('sha512', $password . $random_salt);
-
-		if (isset($_POST['email'], $_POST['password'], $_POST['mcusername'])){
+		if (isset($_POST['planname'], $_POST['maxslots'], $_POST['storage'], $_POST['memory'])){
 			createUser($_POST['email'], $_POST['password'], $_POST['mcusername'], $salt);
 			header("Location: ../index.php");
 			die();
 		}
 	?>
 	<div class="well">
-        <h2> Create User </h2>
-	<form action="./includes/createuser.inc.php" method="post" name="createuser_form">
-		Minecraft Username:
-	        <br><input type="text" name = "mcusername" class="form-control" placeholder="Minecraft Username" autofocus>
-	        Email:
-	        <br><input type="email" name= "email" class="form-control" placeholder="Email address" required>
-	        Password:
-	        <br><input type="password" name="password" class="form-control" placeholder="Password" required>
-	        Password Confirmation:
-		<br><input type="password" name="confirmpwd" class="form-control" placeholder="Confirmation Password" required>
-		<br><input class="btn btn-lg btn-success btn-block" type="submit" value="Create User"/>
+        <h2> Create Plan </h2>
+	<form action="./includes/createplan.inc.php" method="post" name="createplan_form">
+		Plan Name:
+	        <br><input type="text" name = "planname" class="form-control" autofocus>
+                Max Slots:
+                <br><input type="text" name = "maxslots" class="form-control" autofocus>
+                Storage (MB):
+                <br><input type="text" name = "storage" class="form-control" autofocus>
+                Memory (MB):
+                <br><input type="text" name = "memory" class="form-control" autofocus>
+		<br><input class="btn btn-lg btn-success btn-block" type="submit" value="Create Plan"/>
 	</form>
 </div>
