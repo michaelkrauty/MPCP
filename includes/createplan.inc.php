f0171d31d1336f3c397ec99d87c75aba12d5b929
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-11 03:52:35 +0000

    new file:   includes/createplan.inc.php

---
+++ includes/createplan.inc.php
@@ -0,0 +1,24 @@
+	<?php
+		include_once 'db_connect.php';
+		$error_msg = "";
+
+		if (isset($_POST['planname'], $_POST['maxslots'], $_POST['storage'], $_POST['memory'])){
+			createUser($_POST['email'], $_POST['password'], $_POST['mcusername'], $salt);
+			header("Location: ../index.php");
+			die();
+		}
+	?>
+	<div class="well">
+        <h2> Create Plan </h2>
+	<form action="./includes/createplan.inc.php" method="post" name="createplan_form">
+		Plan Name:
+	        <br><input type="text" name = "planname" class="form-control" autofocus>
+                Max Slots:
+                <br><input type="text" name = "maxslots" class="form-control" autofocus>
+                Storage (MB):
+                <br><input type="text" name = "storage" class="form-control" autofocus>
+                Memory (MB):
+                <br><input type="text" name = "memory" class="form-control" autofocus>
+		<br><input class="btn btn-lg btn-success btn-block" type="submit" value="Create Plan"/>
+	</form>
+</div>
