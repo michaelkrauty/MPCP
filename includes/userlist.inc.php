35ff92d9fd8b3fc5a7ffb7634f9be24a5468ba93
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-10 23:14:32 -0800

    Update userlist.inc.php

--- includes/userlist.inc.php
+++ includes/userlist.inc.php
@@ -4,7 +4,7 @@
 <div id='userlist' style='background:white;'>
 <table class="table table-hover table-bordered">
 <tr>
-<th width="20">UID</th>
+<th width="20px">UID</th>
 <th width="5%">Avatar</th>
 <th width="22%">Email</th>
 <th width="20%">Password</th>
