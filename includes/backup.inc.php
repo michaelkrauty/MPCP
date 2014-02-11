47742b95b9516bf16c6077c6a955606690b25d85
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-11 11:11:03 -0800

    Update backup.inc.php

--- includes/backup.inc.php
+++ includes/backup.inc.php
@@ -1,8 +1,9 @@
-                  <?php include_once "mpcp.inc.php";
-                    if(count($_POST) > 0 && isset($_POST['backup'])) {
-                      $serverId = "mpcp_0";
-                      backupServer($serverId);
-                    }
-                  ?>
-                  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>"><button name="backup" type="submit" class='btn btn-lg btn-normal'>Backup</button>
-                  </form>
+<?php include_once "mpcp.inc.php";
+  date_default_timezone_set("America/Los_Angeles");
+  if(count($_POST) > 0 && isset($_POST['backup'])) {
+    $serverId = "mpcp_0";
+    backupServer($serverId);
+  }
+?>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>"><button name="backup" type="submit" class='btn btn-lg btn-normal'>Backup</button>
+</form>
