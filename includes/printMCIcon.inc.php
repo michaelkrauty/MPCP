f1d5d70a08b4d14873774ee25514cd7e36ee078b
Daniel Oxenbury <Doxy5528@hotmail.com>
2014-02-11 06:43:41 +0000

    modified:   includes/header.inc.php 	modified:   includes/printMCIcon.inc.php 	modified:   includes/userwelcome.inc.php

--- includes/printMCIcon.inc.php
+++ includes/printMCIcon.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-function MCIcon($mcusername) {
+function MCIconSmall($mcusername) {
 
   $image1 = "http://signaturecraft.us/avatars/30/face/" . $mcusername . ".png";
   $image2 = "http://signaturecraft.us/avatars/30/face/noskin.png";
@@ -12,4 +12,29 @@ function MCIcon($mcusername) {
   echo "<img src='http://signaturecraft.us/avatars/30/face/" . $mcusername . ".png' height='32' width='32'>";
   }
 }
+function MCIconMedium($mcusername) {
+
+  $image1 = "http://signaturecraft.us/avatars/30/face/" . $mcusername . ".png";
+  $image2 = "http://signaturecraft.us/avatars/30/face/noskin.png";
+  $md5image1 = md5(file_get_contents($image1));
+  $md5image2 = md5(file_get_contents($image2));
+  if($md5image1 == $md5image2){
+  echo "<img src='http://signaturecraft.us/avatars/30/face/Herobrine.png' height='64' width='64'>";
+  }else{
+  echo "<img src='http://signaturecraft.us/avatars/30/face/" . $mcusername . ".png' height='64' width='64'>";
+  }
+}
+function MCIconLarge($mcusername) {
+
+  $image1 = "http://signaturecraft.us/avatars/30/face/" . $mcusername . ".png";
+  $image2 = "http://signaturecraft.us/avatars/30/face/noskin.png";
+  $md5image1 = md5(file_get_contents($image1));
+  $md5image2 = md5(file_get_contents($image2));
+  if($md5image1 == $md5image2){
+  echo "<img src='http://signaturecraft.us/avatars/30/face/Herobrine.png' height='128' width='128'>";
+  }else{
+  echo "<img src='http://signaturecraft.us/avatars/30/face/" . $mcusername . ".png' height='128' width='128'>";
+  }
+}
+
 ?>
--- includes/userwelcome.inc.php
+++ includes/userwelcome.inc.php
@@ -1,16 +1,10 @@
 <div class ="well">
 <?php
+	
 	  echo "<h2>Welcome!</h2>";
-          $image1 = "http://signaturecraft.us/avatars/30/face/" . $userName . ".png";
-          $image2 = "http://signaturecraft.us/avatars/30/face/noskin.png";
-            $md5image1 = md5(file_get_contents($image1));
-            $md5image2 = md5(file_get_contents($image2));
-            if($md5image1 == $md5image2){
-              echo "<img src='http://signaturecraft.us/avatars/30/face/Herobrine.png' height='100' width='100'>";
-            }else{
-              echo "<img src='http://signaturecraft.us/avatars/30/face/" . $userName . ".png' height='100' width='100'>";
-            }
-	    echo "<br></br>";
-	    echo shell_exec("fortune");
+	  include_once 'printMCIcon.inc.php';
+	  MCIconLarge($userName);
+	  echo "<br></br>";
+          echo shell_exec("fortune");
           ?>
 </div>
