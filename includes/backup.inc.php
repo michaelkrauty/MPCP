2c259f1cf2cdd59c8e16e8299bdb3b97fde5fb3d
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-12 14:34:00 -0800

    Update backup.inc.php

--- includes/backup.inc.php
+++ includes/backup.inc.php
@@ -3,12 +3,12 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <?php include_once "/var/www/html/mpcp/includes/db_connect.php"; include_once "/var/www/html/mpcp/includes/functions.php";
+    <?php include_once "db_connect.php"; include_once "functions.php";
       sec_session_start();
       if(login_check($mysqli) == true){
       ?>
       <?php
-        include_once "/var/www/html/mpcp/includes/mpcp.inc.php";
+        include_once "mpcp.inc.php";
         $serverId = "mpcp_0";
         $userEmail = $_SESSION['email'];
         $userName = $_SESSION['username'];
@@ -19,20 +19,20 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="description" content="">
       <meta name="author" content="">
-      <link rel="shortcut icon" href="/var/www/html/mpcp/bootstrap/assets/ico/favicon.ico">
+      <link rel="shortcut icon" href="../bootstrap/assets/ico/favicon.ico">
       <!-- Custom styles for this template -->
-      <link href="/var/www/html/mpcp/webroot/css/manage.backup.css" rel="stylesheet">
+      <link href="../webroot/css/manage.backup.css" rel="stylesheet">
       <title>MPCP Backup</title>
       <!-- Bootstrap theme -->
-      <link href='/var/www/html/mpcp/bootstrap/dist/css/bootstrap-theme.min.css' rel='stylesheet'>
+      <link href='../bootstrap/dist/css/bootstrap-theme.min.css' rel='stylesheet'>
       <!-- Bootstrap core CSS -->
-      <link href="/var/www/html/mpcp/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+      <link href="../bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
-      <?php include_once "/var/www/html/mpcp/includes/header.inc.php"; ?>
+      <?php include_once "header.inc.php"; ?>
         <div class="container-fluid">
           <div class="row">
-          <?php include_once "/var/www/html/mpcp/includes/sidebar.inc.php"; ?>
+          <?php include_once "sidebar.inc.php"; ?>
             <div class="col-sm-9 col-sm-offset-4 col-md-8 col-md-offset-0 main">
               <div class="bar1">
                 <div class="well">
@@ -59,11 +59,11 @@
         ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
         <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-        <script src="/var/www/html/mpcp/bootstrap/dist/js/bootstrap.min.js"></script>
-        <script src="/var/www/html/mpcp/bootstrap/assets/js/docs.min.js"></script>
+        <script src="./bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="../bootstrap/assets/js/docs.min.js"></script>
         <script type="text/javascript">
           var auto_refresh = setInterval( function (){
-          $('#status').load('/var/www/html/mpcp/includes/status.inc.php');
+          $('#status').load('status.inc.php');
           }, 1000);
         </script>
       </body>
