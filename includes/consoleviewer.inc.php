6d63945d93fa484e46c744ffa43e0fe3cb8698df
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-13 12:38:12 -0800

    Update consoleviewer.inc.php

--- includes/consoleviewer.inc.php
+++ includes/consoleviewer.inc.php
@@ -1,2 +1,2 @@
 <?php $text = file_get_contents("/var/mpcp/servers/mpcp_0/logs/latest.log"); ?>
-<textarea name="text" style="width:80%;height:80%;"><?php echo htmlspecialchars($text); ?></textarea>
+<textarea name="text"><?php echo htmlspecialchars($text); ?></textarea>
