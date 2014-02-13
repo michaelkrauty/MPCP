2b4478031edd5ebb2ca6560ac56f1aa7c1e47fd1
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-13 12:18:33 -0800

    Update consoletest.php

--- consoletest.php
+++ consoletest.php
@@ -4,17 +4,6 @@
 $url = 'http://dominationvps.com/mpcp/edittest.php';
 $file = '/var/mpcp/servers/mpcp_0/logs/latest.log';
 
-// check if form has been submitted
-if (isset($_POST['text'])){
-    // save the text contents
-    file_put_contents($file, $_POST['text']);
-
-    // redirect to form again
-    header(sprintf('Location: %s', $url));
-    printf('<a href="%s">Moved</a>.', htmlspecialchars($url));
-    exit();
-}
-
 // read the textfile
 $text = file_get_contents($file);
 
