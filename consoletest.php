f266176f16af5219d8f6f4dc0931ed1367a490fd
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-13 12:26:17 -0800

    Update consoletest.php

--- consoletest.php
+++ consoletest.php
@@ -1,3 +1,2 @@
 <?php $text = file_get_contents("/var/mpcp/servers/mpcp_0/logs/latest.log"); ?>
-<textarea name="text" style="width:100%;height:50%;"><?php echo htmlspecialchars($text) ?></textarea>
-<?php echo htmlspecialchars($text) ?>
+<textarea name="text" style="width:100%;height:100%;"><?php echo htmlspecialchars($text); ?></textarea>
