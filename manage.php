a776adab0e745c5929d63192ff7a82ed2a88a285
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-13 10:30:35 -0800

    Update manage.php

--- manage.php
+++ manage.php
@@ -66,10 +66,10 @@
                     }
                   ?>
                   <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                  <button name="start" type="submit" class='btn btn-lg btn-success' <?php while(screenOnline($serverId)){echo "disabled='disabled'";}?>>Start</button>
-                  <button name="stop" type="submit" class='btn btn-lg btn-danger' <?php while(!screenOnline($serverId)){echo "disabled='disabled'";}?>>Stop</button>
-                  <button name="restart" type="submit" class='btn btn-lg btn-warning' <?php while(!screenOnline($serverId)){echo "disabled='disabled'";}?>>Restart</button>
-                  <button name="reload" type="submit" class='btn btn-lg btn-info' <?php while(!screenOnline($serverId)){echo "disabled='disabled'";}?>>Reload</button>
+                  <button name="start" type="submit" class='btn btn-lg btn-success' <?php if(screenOnline($serverId)){echo "disabled='disabled'";}?>>Start</button>
+                  <button name="stop" type="submit" class='btn btn-lg btn-danger' <?php if(!screenOnline($serverId)){echo "disabled='disabled'";}?>>Stop</button>
+                  <button name="restart" type="submit" class='btn btn-lg btn-warning' <?php if(!screenOnline($serverId)){echo "disabled='disabled'";}?>>Restart</button>
+                  <button name="reload" type="submit" class='btn btn-lg btn-info' <?php if(!screenOnline($serverId)){echo "disabled='disabled'";}?>>Reload</button>
                   </form>
                   </div>
                 </div>
