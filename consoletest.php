9d80f662199b171ef89824ce49978844d37a1819
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-13 12:19:44 -0800

    Update consoletest.php

--- consoletest.php
+++ consoletest.php
@@ -1,16 +1,4 @@
 <?php
-
-// configuration
-$url = 'http://dominationvps.com/mpcp/edittest.php';
-$file = '/var/mpcp/servers/mpcp_0/logs/latest.log';
-
-// read the textfile
-$text = file_get_contents($file);
-
+$text = file_get_contents("/var/mpcp/servers/mpcp_0/logs/latest.log");
 ?>
-<!-- HTML form -->
-<form action="" method="post">
 <textarea name="text" style="width:100%;height:92%;"><?php echo htmlspecialchars($text) ?></textarea>
-<input type="submit" />
-<input type="reset" />
-</form>
