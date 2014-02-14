7b1e49b253553bed6468949275ba17404bfe0068
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-14 12:49:57 -0800

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,21 @@
 <?php
-$jar = "spigot-1.7.4-R0.3-SNAPSHOT.jar";
-#$jar = "craftbukkit-1.7.2-R0.3-20131225.043511-4.jar";
-if("craftbukkit-" == substr($jar, 0, 12)){
-  echo substr($jar, 12, 5);
-}
-if("spigot-" == substr($jar, 0, 7)){
-  echo substr($jar, 7, 5);
-}
+	function getJarVersion($jar){
+		if("craftbukkit-" == substr($jar, 0, 12)){
+			return substr($jar, 12, 5);
+		}
+		if("spigot-" == substr($jar, 0, 7)){
+			return substr($jar, 7, 5);
+		}
+		if("minecraft_server." == substr($jar, 0, 17)){
+			return substr($jar, 12, 5);
+		}
+	}
+	
+	echo getJarVersion("craftbukkit-1.6.4-R2.0.jar");
+	echo "<br>";
+	echo getJarVersion("minecraft_server.1.7.4.jar");
+	echo "<br>";
+	echo getJarVersion("spigot-1.7.2-R0.3-SNAPSHOT.jar");
 
 
 ?>
