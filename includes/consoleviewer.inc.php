34c1eb7f9c1c3e3458b189e099cf6456d2b49825
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-14 10:47:31 -0800

    Update consoleviewer.inc.php

--- includes/consoleviewer.inc.php
+++ includes/consoleviewer.inc.php
@@ -1,2 +1,5 @@
 <?php $text = file_get_contents("/var/mpcp/servers/mpcp_0/logs/latest.log"); ?>
 <textarea name="text"><?php echo htmlspecialchars($text); ?></textarea>
+<script>
+  textbox.scroll = textbox.maxScroll;
+</script>
