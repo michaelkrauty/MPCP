795ff6e8bd40a14384d2115668059e611ed65810
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-14 12:45:35 -0800

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
-#$jar = "spigot-1.7.2-R0.3-SNAPSHOT.jar";
-$jar = "craftbukkit-1.7.2-R0.3-20131225.043511-4.jar";
+$jar = "spigot-1.7.2-R0.3-SNAPSHOT.jar";
+#$jar = "craftbukkit-1.7.2-R0.3-20131225.043511-4.jar";
 if("craftbukkit-" == substr($jar, 0, 12)){
   echo substr($jar, 12, 5);
 }
