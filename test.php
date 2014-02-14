40ff3ad8b2686e56e10b40147e17a3665c2881f0
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-14 12:27:42 -0800

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
 
 
-if("craftbukkit-" = substr($jar, 0, 12)){
-  if("craftbukkit-" = substr("craftbukkit-1.7.2-R0.3-20131225.043511-4.jar", 0, 12)){
+if("craftbukkit-" == substr($jar, 0, 12)){
+  if("craftbukkit-" == substr("craftbukkit-1.7.2-R0.3-20131225.043511-4.jar", 0, 12)){
     echo "yep";
   }else{
     echo "nope";
