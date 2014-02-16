a9d0be937daab808ba9bfd775bbba030a1d9f7f1
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-16 09:32:12 -0800

    Update backup.inc.php

--- includes/backup.inc.php
+++ includes/backup.inc.php
@@ -1,27 +1,29 @@
-<?php
-  if(isset($_POST['backupname'])){
-    $backupName = $_POST['backupname'].".zip";
-  }
-  if(!isset($_POST['backupname'])){
-    $backupName = "testbackup.zip";
-  }
-  if(count($_POST) > 0 && isset($_POST['backup'])){
-    createBackup($serverId);
-  }
-  if(count($_POST) > 0 && isset($_POST['restore'])){
-    stopServer($serverId);
-    while(screenOnline($serverId)){
-      sleep(0);
+<div class="well">
+  <?php
+    if(isset($_POST['backupname'])){
+      $backupName = $_POST['backupname'].".zip";
     }
-    restoreBackup($serverId, "testbackup.zip");
-  }
-  if(count($_POST) > 0 && isset($_POST['delete']) && isset($_POST['backupname'])){
-    deleteBackup($serverId, $_POST['backupname']);
-  }
-?>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-  <input name="backupname" type="text"></input><br>
-  <button name="backup" type="submit" class='btn btn-lg btn-success'>Backup</button>
-  <button name="restore" type="submit" class="btn btn-lg btn-warning" >Restore</button>
-  <button name="delete" type="submit" class="btn btn btn-danger">Delete Backup</button>
-</form>
+    if(!isset($_POST['backupname'])){
+      $backupName = "testbackup.zip";
+    }
+    if(count($_POST) > 0 && isset($_POST['backup'])){
+      createBackup($serverId);
+    }
+    if(count($_POST) > 0 && isset($_POST['restore'])){
+      stopServer($serverId);
+      while(screenOnline($serverId)){
+        sleep(0);
+      }
+      restoreBackup($serverId, "testbackup.zip");
+    }
+    if(count($_POST) > 0 && isset($_POST['delete']) && isset($_POST['backupname'])){
+      deleteBackup($serverId, $_POST['backupname']);
+    }
+  ?>
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <input name="backupname" type="text"></input><br>
+    <button name="backup" type="submit" class='btn btn-lg btn-success'>Backup</button>
+    <button name="restore" type="submit" class="btn btn-lg btn-warning" >Restore</button>
+    <button name="delete" type="submit" class="btn btn btn-danger">Delete Backup</button>
+  </form>
+</div>
