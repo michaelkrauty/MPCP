02107efe7e0e00511458fe9714d8dd5cd2a45dc0
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-16 09:41:04 -0800

    Update edittest.inc.php

--- includes/edittest.inc.php
+++ includes/edittest.inc.php
@@ -1,16 +1,19 @@
-<?php
-  $url = 'http://dominationvps.com/mpcp/includes/edittest.inc.php';
-  $file = '/var/mpcp/servers/'.$serverId.'/logs/latest.log';
-  if (isset($_POST['text'])){
-    file_put_contents($file, $_POST['text']);
-    header(sprintf('Location: %s', $url));
-    printf('<a href="%s">Moved</a>.', htmlspecialchars($url));
-    exit();
-  }
-  $text = file_get_contents($file);
-?>
-<form action="" method="post">
-  <textarea name="text" style="width:100%;height:92%;"><?php echo htmlspecialchars($text) ?></textarea>
-  <input type="submit"/>
-  <input type="reset"/>
-</form>
+<div class="well">
+  <h2>Log Viewer/Editor</h2>
+  <?php
+    $url = 'http://dominationvps.com/mpcp/includes/edittest.inc.php';
+    $file = '/var/mpcp/servers/'.$serverId.'/logs/latest.log';
+    if (isset($_POST['text'])){
+      file_put_contents($file, $_POST['text']);
+      header(sprintf('Location: %s', $url));
+      printf('<a href="%s">Moved</a>.', htmlspecialchars($url));
+      exit();
+    }
+    $text = file_get_contents($file);
+  ?>
+  <form action="" method="post">
+    <textarea name="text" style="width:100%;height:92%;"><?php echo htmlspecialchars($text) ?></textarea>
+    <input type="submit"/>
+    <input type="reset"/>
+  </form>
+</div>
