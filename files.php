c621da5d4923369c82adad00538dea5f12ff6bd4
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-21 15:04:45 -0800

    Update files.php

--- files.php
+++ files.php
@@ -1,1 +1,61 @@
-<?php?>
+<?php include_once "includes/db_connect.php"; include_once "includes/functions.php";
+  sec_session_start();
+  if(login_check($mysqli) == true){
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Daniel Oxenbury (http://daniel.oxituk.co.uk)">
+    <meta name="author" content="Michael Krautkramer">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+    <title>MPCP Home</title>
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="webroot/css/files.css" rel="stylesheet">
+    <?php
+      $userEmail = $_SESSION['email'];
+      $userName = $_SESSION['username'];
+      $pageName = "files";
+    ?>
+</head>
+  <?php include_once "./includes/header.inc.php"; ?>
+  <body role='document'> <body role='document'>
+  <div class='container theme-showcase' role='main'>
+    <div class='jumbotron'>
+      <div class='page-header'>
+        <center>
+          lel
+	      </center>
+      </div>
+    </div>
+  </div>
+  </body>
+  <?php
+    }else{
+    ?>
+    <script>
+      alert("You must be logged in to view this page!");
+    </script>
+    <?php
+      header("Location: http://dominationvps.com/mpcp/login.php");
+    }
+  ?>
+  <?php include_once "./includes/footer.inc.php"; ?>
+
+</html>
+    <!-- ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="./bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="./bootstrap/assets/js/docs.min.js"></script>
+    <script src="./webroot/js/Chart.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script>
