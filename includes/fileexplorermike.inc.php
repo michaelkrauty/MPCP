3038f6ab154ec24ca333328a9a1ca0d77e463dd6
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-22 21:03:56 -0800

    Update fileexplorermike.inc.php

--- includes/fileexplorermike.inc.php
+++ includes/fileexplorermike.inc.php
@@ -1,20 +1,12 @@
 <div class="well">
-<h2> File Explorer Mike</h2>
+  <h2> File Explorer Mike</h2>
 
-<?php
-/**
-<div class="btn-group">
-<button type="button" class="btn btn-default"></span> Zip</button>
-<button type="button" class="btn btn-default"></span> Unzip</button>
-</div>
-**/
-?>
-<br>
 <?php
 
 $serverId = "mpcp_0";
 
 $ls = shell_exec("ls /var/mpcp/servers/$serverId");
+$out = str_replace("\n", "<br>" $ls)
 echo $ls;
 
 ?>
