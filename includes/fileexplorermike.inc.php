9298f2c2e3ad42096aafade567200b5850aacdb4
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-22 16:07:13 -0800

    Update fileexplorermike.inc.php

--- includes/fileexplorermike.inc.php
+++ includes/fileexplorermike.inc.php
@@ -1,21 +1,6 @@
 <div class="well">
-<h2> File Explorer </h2>
-<div class="well">
-<div class="btn-group">
-<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-home"></span></button>
-<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-refresh"></span></button>
-<button type="button" class="btn btn-default">New Folder</button>
-</div>
-<div class="btn-group">
-<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-plus"></span></button>
-<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-minus"></span></button>
-<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-wrench"></span></button>
-<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-font"></span></button>
-</div>
-<div class="btn-group">
-<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-open"></span></button>
-<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-save"></span></button>
-</div>
+<h2> File Explorer Mike</h2>
+
 <?php
 /**
 <div class="btn-group">
@@ -33,3 +18,4 @@ $ls = shell_exec("ls /var/mpcp/servers/$serverId");
 echo $ls;
 
 ?>
+</div>
