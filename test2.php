0f309dd4bf90836ef9fa2e57b83ae16179f1a25c
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-27 22:33:07 -0800

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,3 +1,4 @@
+<body onLoad='scroll()'>
 <div style="width:100%;margin:3px 0;">
   <div style="height:95%;border:1px solid #C9C9C9;border-radius:3px;-moz-border-radius:3px;-webkit-border-radius:3px;color:#939391;background:#272922;padding:4px 2px 4px 4px;">
     <div style="overflow-y:scroll;overflow-x:auto;height:100%;">
