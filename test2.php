18df33dc20c2902adf677f7e2c73e4c01f492faf
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-27 22:49:48 -0800

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,4 +1,3 @@
-<body onLoad='scroll()'>
 <div style="width:100%;margin:3px 0;">
   <div style="height:95%;border:1px solid #C9C9C9;border-radius:3px;-moz-border-radius:3px;-webkit-border-radius:3px;color:#939391;background:#272922;padding:4px 2px 4px 4px;">
     <div style="overflow-y:scroll;overflow-x:auto;height:100%;">
@@ -17,4 +16,3 @@
     $('#echo').load("echo.inc.php");
     }, 1000);
 </script>
-</body>
